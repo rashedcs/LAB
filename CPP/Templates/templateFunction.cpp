ee149428255e3e7d0273328b604be933d3ac97b0
Rashed <rashedcs@users.noreply.github.com>
2017-04-06 09:53:41 +0600

    Add files via upload

---
+++ CPP/Templates/templateFunction.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T>
+void swapargs(T a, T b)
+{
+  T temp = a;
+  a = b;
+  b = temp;
+  cout<<a<<" "<<b<<endl;
+}
+
+
+int main()
+{
+  int a, b;
+  float x, y;
+  string str1, str2;
+
+  swapargs(2,3);
+  swapargs(2.5,3.5);
+  swapargs("Rashed","Bash");
+
+  return 0;
+}
