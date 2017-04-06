5036ccdf016cb529b053dc48c20c6d426e5b5055
Rashed <rashedcs@users.noreply.github.com>
2017-04-06 09:54:20 +0600

    Add files via upload

---
+++ CPP/Virtual Functions/virtualFunction.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Base
+{
+  public:
+  virtual void show()
+  {
+    cout << "Base class";
+  }
+};
+
+class Derived:public Base
+{
+  public:
+  void show()
+  {
+    cout << "Derived Class";
+  }
+};
+
+
+int main()
+{
+ ios::sync_with_stdio(false);
+ Base* b;       //Base class pointer
+ Derived d;     //Derived class object
+ b = &d;
+ b->show();     //Late Binding Ocuurs
+}
+
