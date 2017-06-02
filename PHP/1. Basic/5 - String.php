372f39e4bf3cdc934dc9e99c8473c8654a493b9f
Rashed <rashedcs@users.noreply.github.com>
2017-06-02 23:48:08 +0600

    Create 5 - String.php

---
+++ PHP/1. Basic/5 - String.php
@@ -0,0 +1,8 @@
+Get The Length of a String
+The PHP strlen() function returns the length of a string.
+
+The example below returns the length of the string "Hello world!":
+
+<?php
+echo strlen("Hello world!"); // outputs 12
+?>
