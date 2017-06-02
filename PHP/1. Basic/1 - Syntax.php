c7c99c24b783565c88b3bb025415f178aedc2f49
Rashed <rashedcs@users.noreply.github.com>
2017-06-02 22:22:03 +0600

    Create 1 - Syntax.php

---
+++ PHP/1. Basic/1 - Syntax.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+    
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+?>
+
+</body>
+</html>
