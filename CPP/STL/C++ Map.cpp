b164c58c518d0fb934f2573c16a5c54e455f840c
Rashed <rashedcs@users.noreply.github.com>
2017-06-20 13:18:52 +0600

    Update C++ Map.cpp

--- CPP/STL/C++ Map.cpp
+++ CPP/STL/C++ Map.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+        #include <bits/stdc++.h>
 	using namespace std;
 
 
@@ -12,7 +12,7 @@
 		for (int i=0; i<n; i++)
 		{
 			cin>>t;
-			mp[t]++;
+			mp[t]++;  //Increasing particular index like : 1 2 2 3 then map : 1:1 ; 2:3
 		}
 
 	       for(auto val : mp )
@@ -33,55 +33,56 @@
 
 
 
-#include<bits/stdc++.h>
-using namespace std;
+	#include<bits/stdc++.h>
+	using namespace std;
 
-/*
-bool cmp(const pair<int, int>  &p1, const pair<int, int> &p2)
-{
-    return p1.second < p2.second;
-}
-*/
+	/*
+	bool cmp(const pair<int, int>  &p1, const pair<int, int> &p2)
+	{
+	    return p1.second < p2.second;
+	}
+	*/
 
-int main()
-{
-    ios::sync_with_stdio(false);
+	int main()
+	{
+	    ios::sync_with_stdio(false);
 
-    map<int, int> m;
+	    map<int, int> m;
 
-    for(int i=0; i<10; i++)
-    {
-       m[i] = 5*i;
-    }
+	    for(int i=0; i<10; i++)
+	    {
+	       m[i] = 5*i;
+	    }
 
-    vector<pair<int, int> > v(m.begin(), m.end());
+	    vector<pair<int, int> > v(m.begin(), m.end());
 
-    /*
-        vector<pair<int, int> > v;
-        copy(m.begin(), m.end(), back_inserter(v));
-        sort(v.begin(), v.end(), cmp);
-    */
+	    /*
+		vector<pair<int, int> > v;
+		copy(m.begin(), m.end(), back_inserter(v));
+		sort(v.begin(), v.end(), cmp);
+	    */
 
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i].first<< " : " <<v[i].second<<endl;
-    }
+	    for(int i=0; i<v.size(); i++)
+	    {
+		cout<<v[i].first<< " : " <<v[i].second<<endl;
+	    }
 
-    return 0;
-}
-
-
-/** copy map key to vector:  *//
-map<int, int>m;
-vector<int>keys;
-transform(m.begin(), m.end(), back_inserter(keys), [](std::pair<int, int> p) { return p.first;} );
+	    return 0;
+	}
 
 
 
-/** copy map value to vector:  *//
-map<int, int>m;
-vector<int>keys;
-transform(m.begin(), m.end(), back_inserter(keys), [](std::pair<int, int> p) { return p.second;} );
+	/** copy map key to vector:  **/
+	map<int, int>m;
+	vector<int>keys;
+	transform( m.begin(), m.end(), back_inserter(keys), [](std::pair<int, int> p) { return p.first;} );
+
+
+
+	/** copy map value to vector:  **/
+	map<int, int>m;
+	vector<int>keys;
+	transform( m.begin(), m.end(), back_inserter(keys), [](std::pair<int, int> p) { return p.second;} );
 
 
 
