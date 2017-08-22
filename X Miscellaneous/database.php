7589dd5ae973e2ce2682bdd5fcf2a1f39354efb9
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-08-22 10:20:42 +0600

    Update database.php

--- X Miscellaneous/database.php
+++ X Miscellaneous/database.php
@@ -1,6 +1,68 @@
 1st way::
 
 
+
+<?php
+
+class database
+{
+
+	public $host = "localhost";
+	public $user = "root";
+	public $pass = "";
+	public $db   = "db_blog";
+	public $link;
+
+
+	public function __construct()
+	{
+		$this->connect();
+	}
+	
+
+	 private function connect()
+	 {
+		$this->link = new mysqli($this->host, $this->user, $this->pass, $this->db);
+		return $this->link;
+      
+	 }
+	 public function select($query)
+	 {
+		$result = $this->link->query($query) or die($this->link->error.__LINE__);
+		if($result->num_rows > 0)
+		{
+			return $result;
+		} 
+		else 
+		{
+			return false;
+		}
+	  }
+
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php 
 Class Database
 {
