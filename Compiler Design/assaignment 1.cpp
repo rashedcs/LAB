c0e071ca7d388cc8d119ec53d85fcc9ba48bef76
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-09-11 23:10:39 +0600

    Update assaignment 1.cpp

--- Compiler Design/assaignment 1.cpp
+++ Compiler Design/assaignment 1.cpp
@@ -34,3 +34,66 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+#include <iostream>
+#include<vector>
+#include<cctype>
+
+using namespace std;
+
+vector<string>ob,ob2;
+string arr[] ={"int","double","auto","char","for","if","else","while","return",
+"string","using","namespace","extern","true","false","continue","break","typedef","float"};
+
+int main()
+{
+    int l = sizeof(arr)/sizeof(arr[0]) ;
+   string st;
+   char ch[30];
+   getline(cin,st,'\x1A');
+   int count =0;
+  for(int i=0;i<st.size(); )
+  {
+      if(st[i]==' ')
+      {
+          i++;
+      }
+      else if(isalpha(st[i]))
+      {
+          count = 0;
+          while(isalpha(st[i]) && i<st.size())
+          {
+              ch[count++] = st[i];
+              i++;
+          }
+          ch[count] = '\0';
+          ob.push_back(ch);
+
+      }
+      else
+      {
+          i++;
+      }
+  }
+  for(int i=0;i<ob.size();i++)
+  {
+      for(int j=0;j<l;j++)
+      {
+          if(ob[i]==arr[j])
+          {
+              ob2.push_back(ob[i]);
+          }
+      }
+  }
+  cout<<endl<<"keyword :"<<endl;
+  for(int i=0;i<ob2.size();i++)
+  {
+      cout<<ob2[i]<<endl;
+  }
+    return 0;
+}
+
