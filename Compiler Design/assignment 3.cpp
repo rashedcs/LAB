cc27ed053e4885b7e373dbb3334db5fd3c4b5a03
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-09-11 23:13:39 +0600

    Update assignment 3.cpp

--- Compiler Design/assignment 3.cpp
+++ Compiler Design/assignment 3.cpp
@@ -64,3 +64,197 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+void callspecificdatatype(string st)
+{
+    bool x = true ;
+    for(int i=0;i<st.size();i++)
+    {
+        if(st[i]=='.')
+        {
+            x = false ;
+            break;
+        }
+    }
+    if(x)
+    {
+        cout<<"Integer"<<endl;
+    }
+    else
+    {
+        cout<<"Float"<<endl;
+    }
+}
+int main()
+{
+    string st;
+    string keyword[] = {"do","while","for","int","double","float","if","else","break","continue"};
+    getline(cin,st);
+    bool x;
+    if(st[0]=='"' && st[st.size()-1]=='"' && st.size()!=1)
+    {
+        cout<<"String"<<endl;
+    }
+    else if(st.size()==1 && (st[0]=='+' || st[0]=='-' || st[0]=='*' || st[0]=='/' || st[0]=='<' || st[0]=='>' || st[0]=='!' || st[0]=='='))
+    {
+
+        if(st[0]=='<' || st[0]=='>')
+        {
+            cout<<"Relational operator\n";
+        }
+        else if(st[0]=='=')
+        {
+            cout<<"Assignment Operator\n";
+        }
+        else if(st[0]=='!')
+        {
+            cout<<"Logical Operator\n";
+        }
+        else if(st[0]=='+' || st[0]=='-' || st[0]=='*' || st[0]=='/')
+        {
+           cout<<"Arithmetic Operator"<<endl;
+        }
+    }
+    else if(st.size()==2 && (st=="&&" || st=="||" || st=="!=" || st=="<=" || st==">=" || st=="++" || st=="--"))
+    {
+        if(st=="&&" || st=="||")
+        {
+            cout<<"Logical Operator\n";
+        }
+        else if(st=="++")
+        {
+            cout<<"Increment Operator\n";
+        }
+        else if(st=="--")
+        {
+            cout<<"Decrement Operator\n";
+        }
+        else
+        {
+            cout<<"Relational Operator\n";
+        }
+    }
+    else if(st[0]=='+' || st[0]=='-' || isdigit(st[0]) || st[0]=='.')
+    {
+        if(!isdigit(st[0]))
+        {
+            x = true ;
+            for(int i=1;i<st.length();i++)
+            {
+                if(isdigit(st[i]) || st[i]=='.')
+                {
+                    ;
+                }
+                else
+                {
+                    x = false ;
+                    break ;
+                }
+            }
+            if(x)
+            {
+                callspecificdatatype(st);
+            }
+        }
+        else if(isdigit(st[0]))
+        {
+             x = true ;
+            for(int i=1;i<st.length();i++)
+            {
+                if(isdigit(st[i]) || st[i]=='.')
+                {
+                    ;
+                }
+                else
+                {
+                    x = false ;
+                    break ;
+                }
+            }
+            if(x)
+            {
+                callspecificdatatype(st);
+            }
+
+        }
+    }
+    else
+    {
+        x = false ;
+        int b = 0;
+        int l = sizeof(keyword)/sizeof(keyword[0]);
+        for(int i=0;i<l;i++)
+        {
+            if(keyword[i]==st)
+            {
+                x = true;
+                break;
+            }
+        }
+        if(x)
+        {
+            cout<<"Keyword\n";
+            b = 1;
+        }
+        bool iden = true;
+
+        if(b==0 && isalpha(st[0]))
+        {
+            for(int i=0;i<st.size();i++)
+            {
+                if(isalpha(st[i]) || st[i]=='_' || isdigit(st[i]))
+                {
+                    ;
+                }
+                else
+                {
+                    iden = false;
+                    break;
+                }
+
+            }
+            if(iden)
+            {
+                cout<<"Identifier\n";
+            }
+        }
+
+    }
+    return 0;
+}
+
