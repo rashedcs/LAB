72f050831569703d03da8b8cf1cbe2a7d11c850e
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-11 21:57:26 +0600

    insert table

---
+++ DBMS/Problem 1/insert table.sql
@@ -0,0 +1,70 @@
+insert into department(dept_name, building, budget)
+	   values("Biology", "Watson", 90000);
+
+
+insert into department(dept_name, building, budget)
+	   values("Computer Science", "Taylor", 100000);
+
+insert into department(dept_name, building, budget)
+	   values("Electronics Engineering", "Taylor", 85000);
+
+
+insert into department(dept_name, building, budget)
+	   values("Finance", "Painter", 120000);
+
+
+insert into department(dept_name, building, budget)
+	   values("History", "Painter", 50000);
+
+
+insert into department(dept_name, building, budget)
+	   values("Music", "Packard", 80000);
+
+
+insert into department(dept_name, building, budget)
+	   values("Physics", "Watson", 70000);
+
+
+
+
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("BIO-101", "Intro to Biology","Biology", 4);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("BIO-301", "Genetics","Biology", 4);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("CS-101", "Intro to Computer","Computer Science", 4);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("CS-190", "Game Design","Computer Science", 4);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("EE-101", "Digital Syste,","Electronics Engineering", 3);
+
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("FIN-101", "Investment Banking","Finance", 3);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("HIS-351", "World History","History", 3);
+
+
+ insert into course(course_id, title, dept_name, credits)
+      values("MU-199", "Music Video Production","Music", 3);
+
+
+insert into course(course_id, title, dept_name, credits)
+      values("PHY-101", "Physical Principle","Physics", 4);
+
+
+
+
