37cd2c3a7f91e92529a43b1c7537071726029dd3
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-12 00:02:49 +0600

    Create query - 4.sql

---
+++ DBMS/Problem 1/query - 4.sql
@@ -0,0 +1,6 @@
+--Query : Find all instructors earning the highest salary (there may be more
+than one with the same salary).
+
+select ID, name
+from instructor
+where salary = (select max(salary) from instructor)
