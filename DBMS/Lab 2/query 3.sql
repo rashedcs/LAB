d4f8721ea3fd84f69b8f45592c27d80c226b0614
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-12 23:05:45 +0600

    query 3.sql

---
+++ DBMS/Lab 2/query 3.sql
@@ -0,0 +1,6 @@
+--Find the names of all branches with customers who have an account
+--in the bank and who live in “Harrison”.
+
+select distinct branch name
+from account natural join depositor natural join customer
+where customer city = ’Harrison’
