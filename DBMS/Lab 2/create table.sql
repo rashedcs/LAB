4975caed84181ff0440075f8fc889e30208cc782
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-12 15:02:18 +0600

    create table

---
+++ DBMS/Lab 2/create table.sql
@@ -0,0 +1,73 @@
+-- Creating branch table
+create table branch
+(
+  branch_name varchar(30),
+  branch_city varchar(30),
+  assests     int,
+  primary key(branch_name)
+);
+
+
+
+-- Creating customer table
+create table customer
+(
+  	customer_name VARCHAR(30),
+	customer_street VARCHAR(30),
+	customer_city VARCHAR(30),
+	primary key (customer_name)
+);
+
+
+
+
+-- Creating loan table=
+create table loan
+(
+   	loan_number INT,
+	branch_name VARCHAR(30),
+	amount int,
+	primary key(loan_number),
+	foreign key(branch_name) references branch(branch_name)
+);
+
+
+
+-- Creating borrower table
+create table borrower 
+(
+	customer_name varchar(30),
+	loan_number   int,
+	primary key(customer_name, loan_number),
+	foreign key(customer_name) references customer(customer_name),
+	foreign key(loan_number)   references loan(loan_number)
+);
+
+
+
+
+
+-- Creating account table
+create table account 
+(
+	account_number int,
+	branch_name    varchar(30),
+	balance        int,
+	primary key(account_number),
+	foreign key(branch_name) references branch(branch_name)
+);
+
+
+
+
+
+-- Creating depositor table
+create table depositer 
+(
+	customer_name varchar(30),
+    account_number int,
+	primary key (customer_name, account_number),
+	foreign key (customer_name)  references customer (customer_name),
+	foreign key (account_number) references account (account_number)
+);
+
