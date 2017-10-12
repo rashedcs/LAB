4ae8124b0ca0c7b4a026e65d14919543f5d4cf52
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-12 23:02:18 +0600

    query 2

--- DBMS/Lab 2/query 2.sql
+++ DBMS/Lab 2/query 2.sql
@@ -1,5 +1,13 @@
 --Find the names of all customers who live on the same street and in the same city as "Smith" 
 
+
+--One way of writing the query is as follows.
 select F.customer_name
 from customer F join customer S using(customer_street, customer_city)
 where S.customer_name = 'Smith' ;
+
+
+--Another way of writting the query is as follows
+select F.customer_name
+from customer F join customer S using(customer_street, customer_city)
+where S.customer_street=F.customer_street and S.customer_name = 'Smith' ;
