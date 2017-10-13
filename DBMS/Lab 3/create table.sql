8d81d91bde4688ad05b424456a18be053570139d
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 12:09:55 +0600

    table creation

--- DBMS/Lab 3/create table.sql
+++ DBMS/Lab 3/create table.sql
@@ -1,1 +1,33 @@
 
+
+create table employee
+(
+	employee_name varchar(30),
+    street        varchar(30),
+    city          varchar(30),
+    primary key (employee_name)
+);
+
+create table works
+(
+    employee_name varchar(30),
+    company_name  varchar(30),
+    salary        numeric(6,0),
+    primary key(employee_name)
+);
+
+
+create table company
+(
+  company_name varchar(30),
+  city         varchar(30),
+  primary key(company_name)
+);
+
+
+create table manages
+(
+   employee_name varchar(30),
+   manager_name varchar(30),
+   primary key(employee_name)
+)
