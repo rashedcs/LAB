3af6f876955d3e77d858382c2fae2181bfda727c
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 02:02:35 +0600

    query 6

--- DBMS/Lab 2/query 6.sql
+++ DBMS/Lab 2/query 6.sql
@@ -1,2 +1,5 @@
---Find the customers of the bank who have an account but not a loan.
 
+--Find the names of all branches that have greater assets than those of at least one branches located in Brooklyn.
+
+select branch_name from branch
+where assets > all (select assets from branch where branch_city='Brooklyn');
