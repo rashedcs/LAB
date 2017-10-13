8d5c2cd7adc3756d73a40d238ab47e61d1b2f2a2
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 23:05:36 +0600

    query 1

--- DBMS/Lab 1/query - 1.sql
+++ DBMS/Lab 1/query - 1.sql
@@ -1,6 +1,3 @@
 -- Query 1 : Find titles of courses in Computer Science department that have 3 credits
 
-select title
-from course
-where dept_name = "Computer Science "
-and credits = 3;
+select title from course where dept_name = "Computer Science " and credits = 3;
