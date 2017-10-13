a80b41e8634a4234e8f19329a06fc55d30de8668
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 23:06:43 +0600

    query 4

--- DBMS/Problem 1/query  4.sql
+++ DBMS/Problem 1/query  4.sql
@@ -1,6 +1,4 @@
 --Query : Find all instructors earning the highest salary (there may be more
 than one with the same salary).
 
-select ID, name
-from instructor
-where salary = (select max(salary) from instructor)
+select ID, name from instructor where salary = (select max(salary) from instructor);
