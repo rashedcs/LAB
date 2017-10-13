606349f445dcf705bf780461ff3d4501de958fd5
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 01:36:17 +0600

    Update query 11.sql

--- DBMS/Lab 1/query 11.sql
+++ DBMS/Lab 1/query 11.sql
@@ -1,4 +1,4 @@
 --Find the lowest, across all departments of the per-department maximum salary computed by the preceding query.
 
 selct min(maximum_salary) from (select dept_name, max(salary) as maximum_salary from instructor)
-group by dept_name;
+group by dept_name;     
