8d4e7c8d1ec41820fb163fe72730a70870047fa3
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 00:15:16 +0600

    Create query 8.sql

---
+++ DBMS/Lab 1/query 8.sql
@@ -0,0 +1,5 @@
+-- Find the names of all students who have taken at least one Comp. Sci. course; make sure
+--there are no duplicate name in the result.
+
+select name from student natural join takes natural join course
+where course.dept = 'Computer Science';
