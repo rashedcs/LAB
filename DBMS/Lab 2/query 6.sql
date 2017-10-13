6da9d981ef2d6c2fae6c2c67b1b0c842c664dd31
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 02:03:44 +0600

    query 6

--- DBMS/Lab 2/query 6.sql
+++ DBMS/Lab 2/query 6.sql
@@ -2,4 +2,4 @@
 --Find the names of all branches that have greater assets than those of at least one branches located in Brooklyn.
 
 select branch_name from branch
-where assets > all (select assets from branch where branch_city='Brooklyn');
+where assets > some (select assets from branch where branch_city='Brooklyn');
