549c20a057abc6640a5c20c5486d8f8d3af4d3c7
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 23:16:31 +0600

    query 15

--- DBMS/Lab 3/query  16.sql
+++ DBMS/Lab 3/query  16.sql
@@ -1,4 +1,3 @@
 --Delete all tuples in the works relation for employees of Small Bank Corporation.
 
-delete from works
-where company_name='Small Bank Corporation';
+delete from works where company_name='Small Bank Corporation';
