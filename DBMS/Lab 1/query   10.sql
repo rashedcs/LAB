b73bdcf34da3fa21536664bca8b3cd2344280865
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 01:38:38 +0600

    query 10

--- DBMS/Lab 1/query   10.sql
+++ DBMS/Lab 1/query   10.sql
@@ -1,4 +1,4 @@
 --For each department, find the maximum salary of instructor in that department.
 --You may assume that every department has at least one instructor
 
-select dept_name, max(salary) from instructor group by dept_name;
+select dept_name, max(salary) from instructor group by dept_name;   
