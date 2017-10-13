850bd9814343cdaaa3d779341c1846a4ea6c4494
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 13:57:18 +0600

    query 5

---
+++ DBMS/Lab 3/query 5.sql
@@ -0,0 +1,18 @@
+--Find all companies located in every city in which Small Bank Corporation is located.
+
+
+--Simply way
+select T.company_name from company T
+where (select R.city from company R where R.company_name = T.company_name)
+contains
+(select S.city from company S where S.company_name="Small Bank Corporation");
+
+
+--Standard way
+select S.company_name from company S
+where not exists 
+(
+  (select city from company where company_name = 'Small Bank Corporation')
+  except
+  (select city from company  T where S.company_name = T.company_name) 
+);
