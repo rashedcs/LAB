d1659d549de06a19983374ccdeca2a23b8523392
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-13 12:10:33 +0600

    Update create table.sql

--- DBMS/Lab 3/create table.sql
+++ DBMS/Lab 3/create table.sql
@@ -2,7 +2,7 @@
 
 create table employee
 (
-	employee_name varchar(30),
+    employee_name varchar(30),
     street        varchar(30),
     city          varchar(30),
     primary key (employee_name)
@@ -19,15 +19,15 @@ create table works
 
 create table company
 (
-  company_name varchar(30),
-  city         varchar(30),
-  primary key(company_name)
+    company_name varchar(30),
+    city         varchar(30),
+    primary key(company_name)
 );
 
 
 create table manages
 (
-   employee_name varchar(30),
-   manager_name varchar(30),
-   primary key(employee_name)
+    employee_name varchar(30),
+    manager_name varchar(30),
+    primary key(employee_name)
 )
