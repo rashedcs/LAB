692b96b21c0682fb07dcb22b08137a9ecb99823b
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 00:57:41 +0600

    table creation

--- DBMS/Lab 1/create table.sql
+++ DBMS/Lab 1/create table.sql
@@ -1,3 +1,12 @@
+create table classroom
+(        building		varchar(15),
+	 room_number		varchar(7),
+	 capacity		numeric(4,0),
+	 primary key (building, room_number)
+);
+
+
+
 create table department 
 (
     dept_name varchar(30),
@@ -27,19 +36,6 @@ create table instructor
 );
 
 
-create table section   
-(
-    course_id   varchar(20),
-    sec_id      varchar(20),
-    semester    varchar(30),
-    year        numeric(4,0),
-    building    varchar(20),
-    room_number int,
-    time_slot_id varchar(5),
-    primary key(course_id, sec_id, semester, year)
-);
-
-
 
 create table teaches
 (
@@ -52,6 +48,53 @@ create table teaches
 );
 
 
+/*
+create table section   
+(
+    course_id   varchar(20),
+    sec_id      varchar(20),
+    semester    varchar(30),
+    year        numeric(4,0),
+    building    varchar(20),
+    room_number int,
+    time_slot_id varchar(5),
+    primary key(course_id, sec_id, semester, year)
+);
+
+*/
+
+create table section
+(
+	 course_id		varchar(8), 
+         sec_id			varchar(8),
+	 semester		varchar(6) check (semester in ('Fall', 'Winter', 'Spring', 'Summer')), 
+	 year			numeric(4,0) check (year > 1701 and year < 2100), 
+	 building		varchar(15),
+	 room_number		varchar(7),
+	 time_slot_id		varchar(4),
+	 primary key (course_id, sec_id, semester, year),
+	 foreign key (course_id) references course
+		on delete cascade,
+	 foreign key (building, room_number) references classroom
+		on delete set null
+);
+
+
+
+
+
+create table student
+(        ID			varchar(5), 
+	 name			varchar(20) not null, 
+	 dept_name		varchar(20), 
+	 tot_cred		numeric(3,0) check (tot_cred >= 0),
+	 primary key (ID),
+	 foreign key (dept_name) references department
+		on delete set null
+);
+
+
+
 create table takes
 (
 	s_id int,
@@ -61,4 +104,54 @@ create table takes
         year     numeric(4,0),
         grade    varchar(10)
 );
+
+
+/*
+create table takes
+(        ID			varchar(5), 
+	 course_id		varchar(8),
+	 sec_id			varchar(8), 
+	 semester		varchar(6),
+	 year			numeric(4,0),
+	 grade		        varchar(2),
+	 primary key (ID, course_id, sec_id, semester, year),
+	 foreign key (course_id,sec_id, semester, year) references section
+		on delete cascade,
+	 foreign key (ID) references student
+		on delete cascade
+);
+*/
+
+create table advisor
+(        s_ID			varchar(5),
+	 i_ID			varchar(5),
+	 primary key (s_ID),
+	 foreign key (i_ID) references instructor (ID)
+		on delete set null,
+	 foreign key (s_ID) references student (ID)
+		on delete cascade
+);
+
+create table time_slot
+(        time_slot_id		varchar(4),
+	 day			varchar(1),
+	 start_hr		numeric(2) check (start_hr >= 0 and start_hr < 24),
+	 start_min		numeric(2) check (start_min >= 0 and start_min < 60),
+	 end_hr			numeric(2) check (end_hr >= 0 and end_hr < 24),
+	 end_min		numeric(2) check (end_min >= 0 and end_min < 60),
+	 primary key (time_slot_id, day, start_hr, start_min)
+);
+
+create table prereq
+(
+	 course_id		varchar(8), 
+	 prereq_id		varchar(8),
+	 primary key (course_id, prereq_id),
+	 foreign key (course_id) references course
+		on delete cascade,
+	 foreign key (prereq_id) references course
+);
+
+
+
 
