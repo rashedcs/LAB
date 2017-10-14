f3c50cbe6c8873bb6ed6094df66a90e60c4f4b17
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 21:22:19 +0600

    query 3

---
+++ DBMS/Lab 4/query  3.sql
@@ -0,0 +1,3 @@
+--Update the damage amount for  the car with licence number 'Dhaka K-536985' in the accident with report number 222 to $25000.
+
+update participated set damage_amount=25000 where report_number=222 and licence='Dhaka K-536985';
