7509cd3e4937ae90cc114455f5a035d7c12bb457
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 22:49:10 +0600

    table creation

--- DBMS/Lab 5/create table.sql
+++ DBMS/Lab 5/create table.sql
@@ -1,1 +1,28 @@
+create table member
+(
+  member_number int,
+  name  varchar(30),
+  age  int,
+  primary key(member_number)
+);
 
+
+
+create table book
+(
+  isbn varchar(30),
+  title varchar(30),
+  author varchar(30),
+  publisher varchar(30),
+  primary key(isbn)
+);
+
+
+
+create table borrowed
+(
+  member_number int,
+  isbn   varchar(30),
+  date   date,
+  primary key(member_number, isbn)
+);
