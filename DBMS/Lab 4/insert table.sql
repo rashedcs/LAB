256afa118f32d20564883e59077f4a8ab9518a22
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-14 20:57:34 +0600

    filling table

---
+++ DBMS/Lab 4/insert table.sql
@@ -0,0 +1,26 @@
+-- inserting person table with  values
+insert into person values
+  (1, 'Noor Uddin', 'Pabna'),
+  (2, 'Rashed Hasan', 'Sirajganj'),
+  (3, 'Nazmul Hossain', 'Rajshahi');
+
+
+
+-- inserting car table with  values
+insert into car values
+  ('Pabna H-124536', 'MAZDA', 2012),
+  ('Dhaka K-536985', 'BMW',   2011),
+  ('Raj   R-413698', 'BENZ',  2010);
+  
+  
+  
+  
+  -- inserting accident table with  values
+insert into accident values
+  (111, '2013-01-12', 'Natore'),
+  (222, '2012-05-10', 'Pabna'),
+  (333, '2011-09-14', 'Bogra');
+  
+  
+  
+  
