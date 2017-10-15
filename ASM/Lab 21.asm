79a19afbec38d90d27876857f349ee7d0dbb0d18
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-15 08:43:57 +0600

    traverse array

--- ASM/Lab 21.asm
+++ ASM/Lab 21.asm
@@ -37,3 +37,60 @@
      
     
  MAIN ENDP
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ; Problem : Write a program to traverse the array 
+
+.MODEL SMALL   
+
+.STACK 100H  
+
+.DATA
+    ARR DB 2, 3, 6, 7  
+    
+ 
+.CODE
+MAIN PROC 
+    
+     MOV AX, @DATA   ;initilization of data segment
+     MOV DS, AX         
+              
+              
+     XOR AX, AX      ;clear ax register
+     XOR BX, BX      ;clear base register
+     MOV CX, 4       ;number of elements
+  
+  
+  FOR:  
+     MOV AX, ARR[BX] ;point to the  current index 
+     ADD AX, 48      ;for converting decimal value
+        
+     MOV AH, 2       ;output
+     MOV DL, AX  
+     INT 21H     
+     
+     INC BX
+     ;ADD BX, 1       ;move pointer to the next element 
+     LOOP FOR        ;loop until done
+  
+MAIN ENDP
