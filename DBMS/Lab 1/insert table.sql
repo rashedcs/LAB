dcd91f6ec429b68e185fdbbfaaa2948b1fabbac1
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-16 23:22:22 +0600

    filling tables

--- DBMS/Lab 1/insert table.sql
+++ DBMS/Lab 1/insert table.sql
@@ -100,24 +100,21 @@ insert into section(course_id, sec_id, semester, year, building, room_number, ti
 	
 	
 	
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(10101, "CS-101", 1, "Fall", 2009);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(10101, "CS-315", 1, "Spring", 2010);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(12121, "FIN-201", 1, "Spring", 2010);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(15151, "MU-199", 1, "Spring", 2010);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(22222, "PHY-101", 1, "Fall", 2009);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(32343, "HIS-351", 1, "Spring", 2010);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(76766, "BIO-101", 1, "Summer", 2009);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(76766, "BIO-101", 1, "Summer", 2010);
-insert into teaches(id, course_id, sec_id, semester, year)
-	values(98345, "EE-181", 1, "Spring", 2009);
+insert into teaches values ('10101', 'CS-101', '1', 'Fall', '2009');
+insert into teaches values ('10101', 'CS-315', '1', 'Spring', '2010');
+insert into teaches values ('10101', 'CS-347', '1', 'Fall', '2009');
+insert into teaches values ('12121', 'FIN-201', '1', 'Spring', '2010');
+insert into teaches values ('15151', 'MU-199', '1', 'Spring', '2010');
+insert into teaches values ('22222', 'PHY-101', '1', 'Fall', '2009');
+insert into teaches values ('32343', 'HIS-351', '1', 'Spring', '2010');
+insert into teaches values ('45565', 'CS-101', '1', 'Spring', '2010');
+insert into teaches values ('45565', 'CS-319', '1', 'Spring', '2010');
+insert into teaches values ('76766', 'BIO-101', '1', 'Summer', '2009');
+insert into teaches values ('76766', 'BIO-301', '1', 'Summer', '2010');
+insert into teaches values ('83821', 'CS-190', '1', 'Spring', '2009');
+insert into teaches values ('83821', 'CS-190', '2', 'Spring', '2009');
+insert into teaches values ('83821', 'CS-319', '2', 'Spring', '2010');
+insert into teaches values ('98345', 'EE-181', '1', 'Spring', '2009');
 	
 	
 	
