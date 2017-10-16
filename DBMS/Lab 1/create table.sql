9584b52407d0ae9df71ba75f65e894fa44944d26
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-16 22:56:12 +0600

    Update create table.sql

--- DBMS/Lab 1/create table.sql
+++ DBMS/Lab 1/create table.sql
@@ -1,10 +1,10 @@
 create table classroom
-(        building		varchar(15),
-	 room_number		varchar(7),
-	 capacity		numeric(4,0),
-	 primary key (building, room_number)
+(
+    building varchar(20),
+    room_number    varchar(7),
+    capacity  numeric(4,0),
+    primary key(building, room_number)
 );
-
 
 
 create table department 
