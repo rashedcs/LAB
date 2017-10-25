bae70c22d02c823a548a3455148047c521ed3fa8
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-26 01:10:08 +0600

    query 12

--- DBMS/Lab 3/query  12.sql
+++ DBMS/Lab 3/query  12.sql
@@ -1,4 +1,4 @@
 --. Find the company that has the smallest payroll.
 
 select company_name from works group by company_name
-having sum (salary) <= all (select sum (salary) from works group by company_name);
+having sum(salary) <= all(select sum(salary) from works group by company_name);
