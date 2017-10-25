d1e73bb0def1894c518d80feaaeffc12dfdbb6c2
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-26 00:00:45 +0600

    filling tables

--- DBMS/Lab 3/insert table.sql
+++ DBMS/Lab 3/insert table.sql
@@ -1,1 +1,32 @@
 
+insert into employee(employee_name, street, city) values("John", "Street E", "Boston");
+insert into employee(employee_name, street, city) values("Furrenburge", "Street E", "Boston");
+insert into employee(employee_name, street, city) values("Smith", "Crown Plaza", "Boston");
+insert into employee(employee_name, street, city) values("Piters", "512 Street", "Boston");
+insert into employee(employee_name, street, city) values("Adam", "Silicon Valley", "Califonia");
+insert into employee(employee_name, street, city) values("Manno", "Silicon Valley", "Califonia");
+
+
+insert into works(employee_name, company_name, salary)  values("John", "First Bank Corporation", 3000);
+insert into works(employee_name, company_name, salary)  values("Furrenburge", "First Bank Corporation", 2700);
+insert into works(employee_name, company_name, salary)  values("Smith", "Small Bank Corporation", 2800);
+insert into works(employee_name, company_name, salary)  values("Piters", "Small Bank Corporation", 2600);
+insert into works(employee_name, company_name, salary)  values("Adam", "Google", 5000);
+insert into works(employee_name, company_name, salary)  values("Manno", "Google", 4500);
+
+
+
+insert into company(company_name, city) values("First Bank Corporation", "Boston");
+insert into company(company_name, city) values("Small Bank Corporation", "Boston");
+insert into company(company_name, city) values("Google", "California");
+
+
+
+insert into manages(employee_name, manager_name) values("Furrenburge", "John");
+insert into manages(employee_name, manager_name) values("Piters", "Smith");
+insert into manages(employee_name, manager_name) values("Manno", "Adam");
+
+
+
+
+
