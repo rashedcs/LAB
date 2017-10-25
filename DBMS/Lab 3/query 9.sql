dc2bc62169bb92ec7fd11d3fdeea97bdd481bbfa
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-26 01:04:19 +0600

    query 9

--- DBMS/Lab 3/query 9.sql
+++ DBMS/Lab 3/query 9.sql
@@ -1,3 +1,3 @@
 --Modify the database so that Jones now lives in Newtown. update employee
 
-set city = ’Newton’ where employee_name = ’Jones’;
+update employee set city='Newton' where employee_name='John';
