b97f3ba0d1594cd0c2b963d47981664cf70725a4
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-26 00:19:16 +0600

    query 11

--- DBMS/Lab 3/query  11.sql
+++ DBMS/Lab 3/query  11.sql
@@ -1,5 +1,5 @@
 --Find the names of all employees in this database who live in the same city as the company for which they work 
 
-select E.person_name from Employee as E, Works as W, Company as C 
+select E.employee_name from employee as E, works as W, company as C 
 where E.employee_name=W.employee_name and E.city=C.city and W.company_name=C.company_name;
    
