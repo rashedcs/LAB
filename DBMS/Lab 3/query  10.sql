88e04b4a6f597ce7cde7199504f78336d37b77ff
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-26 00:02:38 +0600

    Update query  10.sql

--- DBMS/Lab 3/query  10.sql
+++ DBMS/Lab 3/query  10.sql
@@ -1,5 +1,5 @@
 --Find the names of all employees who live in the same city and on the same street as do their managers.
 
-select E1.person_name from Employee as E1, employee as E2, manages as M
+select E1.person_name from employee as E1, employee as E2, manages as M
 Where E1.person_name=M.person_name and E2.person_name=M.manager_name and E1.stree=E2.street and E1.city=E2.city;
 
