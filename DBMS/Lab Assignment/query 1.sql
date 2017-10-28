96c28b37d0cc8ca76de29b3d8dac20ef1a2f1aa0
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-28 11:42:53 +0600

    Update query 1.sql

--- DBMS/Lab Assignment/query 1.sql
+++ DBMS/Lab Assignment/query 1.sql
@@ -1,6 +1,6 @@
 --Find the total marks where total = sum of max class test + attendence
 
-select distinct class_test.roll,class_test.name,class_test.ct1,class_test.ct2,class_test.ct3, 
+select distinct class_test.roll, class_test.name, class_test.ct1, class_test.ct2, class_test.ct3, 
 ((ct1+ct2+ct3)-LEAST(ct1,ct2,ct3)) as 'Sum of Max Two',  attendence.atten_mark as 'Attendence Marks', 
 ((attendence.atten_mark)+((ct1+ct2+ct3)-(LEAST(ct1,ct2,ct3)))) as Total 
 from class_test,attendence
