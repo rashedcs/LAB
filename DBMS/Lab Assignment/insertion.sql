6b9a5d6276d42f71b5577196bc3869b9b6751721
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-28 11:40:30 +0600

    dumping data

--- DBMS/Lab Assignment/insertion.sql
+++ DBMS/Lab Assignment/insertion.sql
@@ -22,13 +22,13 @@ insert into attendence values
 --
 
 insert into class_test  values
-(1, 150148, 'Md. Nazmul Hossain', 0, 9, 8),
-(2, 150149, 'Md Jahir Hossain', 4, 5, 6),
-(3, 150153, 'Md. Ashraful Alam', 6, 7, 5),
-(4, 150140, 'habibullah', 4, 7, 6),
-(5, 150130, 'Md. Ashraful islam', 6, 5, 8),
-(6, 150135, 'Rashed Hasan', 6, 7, 6),
+(1, 150148, 'Md. Nazmul', 5, 7, 8),
+(2, 150149, 'Md Jahir ', 3, 6, 5),
+(3, 150153, 'Md. Ashraful ', 6, 7, 5),
+(4, 150140, 'Md. Habibullah', 4, 7, 6),
+(5, 150130, 'Ashraful Islam', 6, 7, 8),
+(6, 150135, 'Rashedul Islam', 6, 7, 6),
 (7, 150126, 'Arafat Islam', 8, 7, 8),
-(8, 150113, 'Md. Jakaria Hossain', 5, 6, 7),
-(9, 150156, 'Md.Deloar Hossain', 7, 6, 6),
-(10, 150154, 'Hablu ', 6, 5, 7);
+(8, 150113, 'Jakaria Hossain', 5, 7, 6),
+(9, 150156, 'Deloar Hossain', 7, 8, 9),
+(10, 150154, 'Hasibur Rahman ', 6, 5, 7);
