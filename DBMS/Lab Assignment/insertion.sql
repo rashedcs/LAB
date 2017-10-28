86b4b508b10c63e7a8f99a2c984ffd6f14d643a3
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-10-28 11:07:23 +0600

    dumping data

---
+++ DBMS/Lab Assignment/insertion.sql
@@ -0,0 +1,35 @@
+--
+-- Dumping data for table `attendence`
+--
+
+insert into attendence values
+(1, 150113, 10),
+(2, 150126, 10),
+(3, 150130, 10),
+(4, 150135, 10),
+(5, 150140, 10),
+(6, 150148, 10),
+(7, 150149, 10),
+(8, 150153, 10),
+(9, 150154, 10),
+(10, 150156, 10);
+
+----------------------------------------------------------
+
+
+
+--
+-- Dumping data for table `ct_mark`
+--
+
+insert into class_test  values
+(1, 150148, 'Md. Nazmul Hossain', 0, 9, 8),
+(2, 150149, 'Md Jahir Hossain', 4, 5, 6),
+(3, 150153, 'Md. Ashraful Alam', 6, 7, 5),
+(4, 150140, 'habibullah', 4, 7, 6),
+(5, 150130, 'Md. Ashraful islam', 6, 5, 8),
+(6, 150135, 'Rashed Hasan', 6, 7, 6),
+(7, 150126, 'Arafat Islam', 8, 7, 8),
+(8, 150113, 'Md. Jakaria Hossain', 5, 6, 7),
+(9, 150156, 'Md.Deloar Hossain', 7, 6, 6),
+(10, 150154, 'Hablu ', 6, 5, 7);
