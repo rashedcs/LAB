342a9ccc6b1937caab0c25bfe4336571ba019dfa
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-02 18:15:00 +0600

    lab 1

--- Compiler Design/lab 1.cpp
+++ Compiler Design/lab 1.cpp
@@ -1,1 +1,53 @@
+//Problem Name : Write a C++ program to find out  keyword from a C/C++ program.
 
+//Solution :
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isKeyword(string buffer)
+{
+    string keywords[32] = {"auto","break","case","char","const","continue","default", "int",
+                            "do","double","else","enum","extern","float","for","goto",
+                            "if","int","long","register","return","short","signed",
+                            "sizeof","static","struct","switch","typedef","union",
+                            "unsigned","void","while"};
+    int flag = 0;
+
+    for(int i=0; i<32; i++)
+    {
+        if(keywords[i]==buffer)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    return flag;
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    ifstream inFile; //Declares a file stream object
+    string word, fileName;
+
+    cout<<"Please enter the file name ";
+    getline(cin,fileName);
+
+    inFile.open(fileName.c_str());
+
+    while(!inFile.eof())
+    {
+        inFile >> word;
+        if(isKeyword(word))
+        {
+           cout<<word<<endl;
+        }
+    }
+    inFile.close();
+    //cin.get();
+    return 0;
+}
