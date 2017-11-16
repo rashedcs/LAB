20a2ac2ff68e1dddfd866f53eb1a54e06ade8032
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-16 22:51:27 +0600

    string reverse

---
+++ ASM/lab 24.asm
@@ -0,0 +1,44 @@
+
+; Problem : input array from user
+INCLUDE 'EMU8086.INC'
+.MODEL SMALL
+.STACK
+.DATA
+   ARR DB 10 DUB (?)     
+   
+.CODE
+ MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    XOR BX, BX
+    MOV CX, 5
+    
+    FOR: 
+      MOV AH, 1
+      INT 21H
+      MOV ARR[BX], AL
+      INC BX
+    LOOP FOR
+    
+    MOV CX, 5
+    MOV DI, 0
+    ADD DI, 4       
+    
+  PRINTN:
+    
+   BACK: 
+     MOV AL, ARR[DI]
+     
+     MOV AH, 2       ;output
+     MOV DL, AL  
+     INT 21H  
+
+     DEC DI      
+   LOOP BACK    
+   
+
+   
+
+    
+ MAIN ENDP
