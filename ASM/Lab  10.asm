834c763c737fa015526ef7b8c4672cc738ddd795
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-16 19:26:59 +0600

    sum greater than 9

--- ASM/Lab  10.asm
+++ ASM/Lab  10.asm
@@ -1,105 +1,42 @@
+
 .model small
-.stack
+.stack 100h
 .data
-a db "enter the first number : $"
-b db "enter the second number: $"
-d db "sum is=$" 
-
+    a db ?
+    b db ?
 
 .code
-
-mov ah,01h
-int 21h
-mov bl,al 
-  
-
-
-mov ah,01h
-int 21h  
-
-
-mov ah,00h
-add al,bl  
-
-
-aaa
-mov bl, al
-mov bh, ah  
-   
-
-;mov dl, bh
-;add dl,30h 
-
-       
-MOV ah, 2
-add bh, 48
-MOV dl, bh
-INT 21H      
+main proc
+    mov ah,1
+    int 21h          
+    mov a,al
     
-        
-mov ah,02 
-add bl, 48
-mov dl,bl  
-int 21h
+    mov ah,1
+    int 21h
+    mov b,al
+    
+    add al,a           ;
+    mov ah,0          
+    aaa               
+    
+    mov bx,ax          
+    add bh,30h
+    add bl,30h
+    
+    mov ah,2
+    mov dl,0ah          ;
+    int 21h
+    mov dl,0dh
+    int 21h
 
+    
+    mov ah,2
+    mov dl,bh           ;showing sum 
+    int 21h  
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
 
-.exit
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-.code
-
-       mov ax,@data
-       mov ds,ax
-       
-       mov ah,9
-       lea dx,msg
-       int 21h
-       
-       mov ah,1 
-       int 21h
-       sub al,48
-       mov a,al
-       
-       mov ah,9
-       lea dx,msg2
-       int 21h
-       
-       mov ah,1
-       int 21h
-       sub al,48
-       mov b,al
-       
-       add al,a
-       mov ah,0
-       aaa
-       
-       add ah,48
-       add al,48
-       
-       mov bx, ax
-       
-       mov ah,9
-       lea dx,msg3
-       int 21h
-       
-       mov ah,2
-       mov dl,bh
-       int 21h
-       
-       mov ah,2
-       mov dl,bl
-       int 21h
-                
-       ret
+main endp
