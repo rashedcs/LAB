2b7ba36ba4e8f61ccd4f3b058db0eaa9bea43840
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-16 15:48:24 +0600

    infix to postfix

--- Compiler Design/lab 9.cpp
+++ Compiler Design/lab 9.cpp
@@ -1,61 +1,43 @@
-#include<stdio.h>
-#include<string>
-#include <locale>
-char stack[20];
-int top = -1;
-void push(char x)
-{
-    stack[++top] = x;
-}
-
-char pop()
-{
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
-}
-
-int priority(char x)
-{
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
-}
+   #include<bits/stdc++.h>
+   using namespace std;
 
 
-int main()
-{
-    char exp[20];
-    char *e, x;
-    printf("Enter the expression :: ");
-    scanf("%s",exp);
-    e = exp;
-    while(*e != '\0')
-    {
-        if(isalnum(*e))
-            printf("%c",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
-        {
-            while((x = pop()) != '(')
-                printf("%c", x);
-        }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
-                printf("%c",pop());
-            push(*e);
-                                }
-               e++;
-        }
-
-    while(top != -1)
-    {
-       printf("%c",pop());
+   void prefex2postfix(string s)
+   {
+      stack<char>st;
+      for(int i=0; i<s.length(); i++)
+      {
+          if(isdigit(s[i]) || isalpha(s[i]))
+          {
+               cout<<s[i];
+          }
+          else if( s[i]==')' )
+          {
+             while(st.top()!='(')
+             {
+                cout<<st.top();
+                st.pop();
+             }
+             st.pop();
+          }
+          else
+          {
+            st.push(s[i]);
+          }
+       }
     }
-}
+
+
+  int main()
+  {
+     ios::sync_with_stdio(false);
+     string str;
+
+     while(cin>>str)
+     {
+        prefex2postfix(str);
+        cout<<endl;
+     }
+
+     return 0;
+  }
