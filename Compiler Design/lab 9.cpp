eb5d65cc771654a26dccf2dc407c18e2acd9894f
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-17 18:21:07 +0600

    infix to postfix

--- Compiler Design/lab 9.cpp
+++ Compiler Design/lab 9.cpp
@@ -1,46 +1,21 @@
-//Reference : mycodeschool.com
-
 #include<bits/stdc++.h>
 using namespace std;
-
-
-
-int GetOperatorWeight(char x)
-{
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
-}
-
-// Function to perform an operation and return output.
-int HasHigherPrecedence(char op1, char op2)
-{
-	int op1Weight = GetOperatorWeight(op1);
-	int op2Weight = GetOperatorWeight(op2);
-
-
-	return op1Weight > op2Weight ?  true: false;
-}
 
 
 string InfixToPostfix(string str)
 {
 	stack<char> S;
-	string postfix = ""; // Initialize postfix as empty string.
+	string postfix="";
 	for(int i = 0; i< str.length(); i++)
 	{
-
         if(isdigit( str[i]) || isalpha( str[i]))
 		{
 			postfix +=  str[i];
 		}
-
 		else if ( str[i] == '(')
 		{
 			S.push( str[i]);
 		}
-
 		else if( str[i] == ')')
 		{
 			while(!S.empty() && S.top() !=  '(')
@@ -52,30 +27,25 @@ string InfixToPostfix(string str)
 		}
 		else
 		{
-			while(!S.empty() && HasHigherPrecedence(S.top(),  str[i]))
-			{
-				postfix += S.top();
-				S.pop();
-			}
 			S.push(str[i]);
 		}
 	}
-
-	while(!S.empty()) {
+	while(!S.empty())
+	{
 		postfix += S.top();
 		S.pop();
 	}
-
 	return postfix;
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+
 	string expression;
-	cout<<"Enter Infix Expression \n";
 	getline(cin,expression);
 	string postfix = InfixToPostfix(expression);
-	cout<<"Output = "<<postfix<<"\n";
+	cout<<postfix<<endl;
 }
 
 
@@ -83,8 +53,5 @@ int main()
 
 
 
-
-
-
-
+//Modfied : https://gist.github.com/mycodeschool/7867739
 //Daught : https://ideone.com/nCW6IF
