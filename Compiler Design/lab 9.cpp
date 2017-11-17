297f99b867e4d44274b3e2bf658a7aea9a4abfc2
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-17 17:19:44 +0600

    Update lab 9.cpp

--- Compiler Design/lab 9.cpp
+++ Compiler Design/lab 9.cpp
@@ -1,43 +1,88 @@
-   #include<bits/stdc++.h>
-   using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
 
-   void prefex2postfix(string s)
-   {
-      stack<char>st;
-      for(int i=0; i<s.length(); i++)
-      {
-          if(isdigit(s[i]) || isalpha(s[i]))
-          {
-               cout<<s[i];
-          }
-          else if( s[i]==')' )
-          {
-             while(st.top()!='(')
-             {
-                cout<<st.top();
-                st.pop();
-             }
-             st.pop();
-          }
-          else
-          {
-            st.push(s[i]);
-          }
-       }
-    }
+
+int GetOperatorWeight(char x)
+{
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+}
+
+// Function to perform an operation and return output.
+int HasHigherPrecedence(char op1, char op2)
+{
+	int op1Weight = GetOperatorWeight(op1);
+	int op2Weight = GetOperatorWeight(op2);
 
 
-  int main()
-  {
-     ios::sync_with_stdio(false);
-     string str;
+	return op1Weight > op2Weight ?  true: false;
+}
 
-     while(cin>>str)
-     {
-        prefex2postfix(str);
-        cout<<endl;
-     }
 
-     return 0;
-  }
+string InfixToPostfix(string str)
+{
+	stack<char> S;
+	string postfix = ""; // Initialize postfix as empty string.
+	for(int i = 0; i< str.length(); i++)
+	{
+
+        if(isdigit( str[i]) || isalpha( str[i]))
+		{
+			postfix +=  str[i];
+		}
+
+		else if ( str[i] == '(')
+		{
+			S.push( str[i]);
+		}
+
+		else if( str[i] == ')')
+		{
+			while(!S.empty() && S.top() !=  '(')
+			{
+				postfix += S.top();
+				S.pop();
+			}
+			S.pop();
+		}
+		else
+		{
+			while(!S.empty() && HasHigherPrecedence(S.top(),  str[i]))
+			{
+				postfix += S.top();
+				S.pop();
+			}
+			S.push(str[i]);
+		}
+	}
+
+	while(!S.empty()) {
+		postfix += S.top();
+		S.pop();
+	}
+
+	return postfix;
+}
+
+int main()
+{
+	string expression;
+	cout<<"Enter Infix Expression \n";
+	getline(cin,expression);
+	string postfix = InfixToPostfix(expression);
+	cout<<"Output = "<<postfix<<"\n";
+}
+
+
+
+
+
+
+
+
+
+
+//Daught : https://ideone.com/nCW6IF
