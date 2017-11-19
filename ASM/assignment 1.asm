5e355c1214c27369ff19fdcea301a31f2fc0ca9e
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-11-19 07:35:17 +0600

    Update assignment 1.asm

--- ASM/assignment 1.asm
+++ ASM/assignment 1.asm
@@ -1,4 +1,65 @@
 ;1+2+3+4+5+.....+n
+;Problem 1 : Write a program to sum of natural numbers
+;series : 1+2+3+......+n
+; Count
+; (first term - las term)/difference 
+
+
+;Source Code
+.MODEL SMALL
+.STACK  
+.DATA
+ b db ?
+.CODE
+MAIN PROC
+MOV AX, 0 
+MOV CX, 10
+
+FOR: 
+ADD AX, CX
+LOOP FOR  
+
+MOV BX, 10    
+
+DIV BX
+MOV b, DX
+
+
+ MOV AH,2
+ MOV DL, AX 
+ ADD DL, 48
+ INT 21H  
+      
+        
+ MOV AH,2
+ MOV DL, b   
+ ADD DL, 48
+ INT 21H
+
+MAIN ENDP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 .MODEL SMALL
 .STACK 100H
