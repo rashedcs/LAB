ccd86f74edb485d8ff6bc9dcdb11b60c469ceb5f
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-01-14 23:36:10 +0600

    Create triangle.cpp

---
+++ Computer Graphics/triangle.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include <windows.h>
+#include <math.h>
+#include <GL\glut.h>
+
+
+void display()
+{
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    glColor3f(1.0, 0.0, 0.0);
+
+                    glBegin(GL_TRIANGLES);
+                      glVertex3f (0, 10, 0.0);
+                      glVertex3f (-10, -10, 0.0);
+                      glVertex3f (10, -10, 0.0);
+                    glEnd();
+
+                    glFlush();
+}
+
+
+void init(void)
+{
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glOrtho(-50.0, 55.0, -50.0, 50.0, -1.0, 1.0);
+}
+
+
+
+int main (int argc, char **argv)
+{
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //
+
+    glutInitWindowSize(755,450);
+
+    glutInitWindowPosition(100, 100);
+
+    glutCreateWindow("Lab 2");
+
+    glutDisplayFunc(display);
+
+    init();
+
+    glutMainLoop();
+
+    return 0;
+}
