f79088ebc6d8d4fe055145fbf38c6e84c1a24b67
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-01-14 22:20:32 +0600

    Update midpoint circle.cpp

--- Computer Graphics/midpoint circle.cpp
+++ Computer Graphics/midpoint circle.cpp
@@ -1,97 +1,85 @@
+#include<bits/stdc++.h>
 #include<windows.h>
-#include <stdio.h>
-#include <iostream>
+#include <GL/glu.h>
 #include <GL/glut.h>
 
-using namespace std;
 
-int pntX1, pntY1, r;
 
-void plot(int x, int y)
+int xCenter,  yCenter,   radius;
+
+
+void putpixel(int x, int y)
 {
 	glBegin(GL_POINTS);
-	glVertex2i(x+pntX1, y+pntY1);
+	glVertex2i(x+xCenter,  y+yCenter);
 	glEnd();
 }
 
 
-void myInit (void)
+void display()
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glPointSize(4.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
-}
+    glClear (GL_COLOR_BUFFER_BIT);
+	glColor3f (1.0, 0.0, 0.0);
+	glPointSize(1.0);
 
+	int   x=0, y = radius;
+	float  p = 1 - radius;
 
-void midPointCircleAlgo()
-{
-	int x = 0;
-	int y = r;
-	float decision = 5/4 - r;
-	plot(x, y);
+    glBegin(GL_POINTS);
+	glVertex2i(x+xCenter, y+yCenter);
+	glEnd();
 
-	while (y > x)
+    while(x<y)
 	{
-		if (decision < 0)
+		if (p<0)
 		{
-			x++;
-			decision += 2*x+1;
+			p += 2*x+1;
 		}
 		else
 		{
 			y--;
-			x++;
-			decision += 2*(x-y)+1;
+			p += 2*(x-y)+1;
 		}
-		plot(x, y);
-		plot(x, -y);
-		plot(-x, y);
-		plot(-x, -y);
-		plot(y, x);
-		plot(-y, x);
-		plot(y, -x);
-		plot(-y, -x);
+		putpixel(x, y);
+		putpixel(x, -y);
+		putpixel(-x, y);
+		putpixel(-x, -y);
+		putpixel(y, x);
+		putpixel(-y, x);
+		putpixel(y, -x);
+		putpixel(-y, -x);
+
+        x++;
 	}
-
+    glFlush ();
 }
 
-void myDisplay(void)
+
+void init()
 {
-	glClear (GL_COLOR_BUFFER_BIT);
-	glColor3f (1.0, 0.0, 0.0);
-	glPointSize(1.0);
-
-	midPointCircleAlgo();
-
-	glFlush ();
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 500, 0, 600);
+    glPointSize(1.0);
 }
+
 
 int main(int argc, char** argv)
 {
-	cout << "Enter the coordinates of the center:\n\n" << endl;
+     xCenter=250;
+     yCenter=250;
+     radius=150;
 
-	/*
-	cout << "X-coordinate   : "; cin >> pntX1;
-	cout << "\nY-coordinate : "; cin >> pntY1;
-	cout << "\nEnter radius : "; cin >> r;
-	*/
-
-    pntX1=250;
-    pntY1=250;
-    r=150;
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize (640, 480);
-	glutInitWindowPosition (100, 150);
-	glutCreateWindow ("Line Drawing Alogrithms");
-	glutDisplayFunc(myDisplay);
-	myInit ();
-	glutMainLoop();
-
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Circle");
+    init ();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
 
 
