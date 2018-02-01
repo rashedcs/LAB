ecdab5313e3991c2e8affe0a0703b688a9a6a5d8
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-02-01 15:28:39 +0600

    Secant Method

--- Numerical Analysis/2.cpp
+++ Numerical Analysis/2.cpp
@@ -1,45 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define epsilon .001
-
-double f(double x)    
+#define EPS 0.001
+double f (double x)
 {
-      return x*x + (-5)*x + 6 ;
+    return x*x + (-5)*x + 6 ;
+}
+double secant()
+{
+    double a=1, b=3, c; //x0=a, x1=b;
+ 
+    while(fabs(b-a)>=EPS)
+    {
+        c = (f(b)*a-f(a)*b) / (f(b)-f(a)) ;
+        a=b;
+        b=c;
+    }
+    return c ;
 }
 
 int main()
-{    
-    double a, b, c, dx;    
-    cin>>a>>b; //3 2
-
-    if(f(a)*f(b)>0)       
-    {             
-            cout<<"Can not determine\n"<<endl;          
-    }
-    else              
-    {
-	    cout<<"a"<<"\t\t\t"<<"b"<<"\t\t\t"<<"c"<<"\t\t\t"<<"f(c)"<<endl;
-	    while (fabs(b-a)>=epsilon)        
-	    {
-		  dx = b-a;
-		  c = a + (dx*=0.5);        
-
-		  cout<<a<<"\t\t\t"<<b<<"\t\t\t"<<c<<"\t\t\t"<<f(c)<<endl;       
-
-		  if(f(c)*f(a)<0)
-		  {
-		      b=c;    
-		  }
-		  else 
-		  {    
-		      a=c;   
-		  }    
-		  //if f(c) is negative 
-		  //(f(c)<0)?  a=c : b=c;   
-	    }
-     }
-       
-    cout<<"The root of the equation is "<<c<<endl;    
-    return 0;    
+{
+    printf("The root is : %.4lf\n",secant());
+    return 0;
 }
-//Daught : https://ideone.com/uUyxYc
+ 
+
+//Daught : https://ideone.com/CSfIPe
