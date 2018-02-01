6a469de36ef77fc6e49abb6f2dc74a723c734828
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-02-01 16:49:46 +0600

    False Position

---
+++ Numerical Analysis/4.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define EPS 0.001
+double f (double x)
+{
+    return x*x*x + (-2)*x - 5 ;
+}
+double secant()
+{
+    double a=2, b=3, c; //x0=a, x1=b;
+ 
+    while(fabs(b-a)>=EPS)
+    {
+        c=(a*f(b)-b*f(a)) / (f(b)-f(a));
+        a=b;
+        b=c;
+    }
+    return c ;
+}
+
+int main()
+{
+    printf("The root is : %.4lf\n",secant());
+    return 0;
+}
