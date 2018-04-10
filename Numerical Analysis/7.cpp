1440d0dcf08ee9ba82dd6bd549ce211ad749f27c
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-04-10 19:39:10 +0600

    Gauess Jorden

--- Numerical Analysis/7.cpp
+++ Numerical Analysis/7.cpp
@@ -1,6 +1,31 @@
 #include<stdio.h>
 
-void solution( int a[][20], int var );
+void solution( int a[ 20 ][ 20 ], int var )
+{
+    int k, i, l, j;
+
+    for ( k = 0;k < var;k++ )
+    {
+        for ( i = 0;i <= var;i++ )
+        {
+            l = a[ i ][ k ];
+
+            for ( j = 0;j <= var;j++ )
+            {
+                if ( i != k )
+                a[i][j] = (a[k][k]*a[i][j])-(l*a[k][j]);
+            }
+        }
+    }
+
+    printf( "\nSolutions:" );
+
+    for ( i = 0;i < var;i++ )
+    {
+        printf( "\nTHE VALUE OF x%d IS %f\n", i + 1, ( float ) a[ i ][ var ] / ( float ) a[ i ][ i ] );
+    }
+
+}
 
 
 int main()
@@ -30,29 +55,3 @@ int main()
 
 
 
-void solution( int a[ 20 ][ 20 ], int var )
-{
-    int k, i, l, j;
-
-    for ( k = 0;k < var;k++ )
-    {
-        for ( i = 0;i <= var;i++ )
-        {
-            l = a[ i ][ k ];
-
-            for ( j = 0;j <= var;j++ )
-            {
-                if ( i != k )
-                a[i][j] = (a[k][k]*a[i][j])-(l*a[k][j]);
-            }
-        }
-    }
-
-    printf( "\nSolutions:" );
-
-    for ( i = 0;i < var;i++ )
-    {
-        printf( "\nTHE VALUE OF x%d IS %f\n", i + 1, ( float ) a[ i ][ var ] / ( float ) a[ i ][ i ] );
-    }
-
-}
