7af0e2ac74c99b2d095e35f8733a3428378047f5
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-04-13 12:14:04 +0600

    Gauess Gorden

--- Numerical Analysis/ 07.cpp
+++ Numerical Analysis/ 07.cpp
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
 
 void solution(int arr[20][20], int n)
 {
     int k, i, l, j;
 
-    for(i=0; i<n; i++ )
+    for(i=1; i<=n; i++ )
     {
-        for(j=0; j<=n; j++ )
+        for(j=1; j<=n; j++ )
         {
             l = arr[j][i];
-            for(k=0; k<=n; k++ )
+            for(k=1; k<=n+1; k++ )
             {
                 if(j!=i)  arr[j][k] = (arr[i][i]*arr[j][k])-(l*arr[i][k]);
             }
@@ -18,7 +18,7 @@ void solution(int arr[20][20], int n)
 
     printf("\nSolutions : \n" );
 
-    for(i=0; i<n; i++ )
+    for(i=1; i<=n; i++)
     {
         printf("x%d = %f\n", i+1, (float)arr[i][n] / (float)arr[i][i]);
     }
@@ -32,9 +32,9 @@ int main()
     int n, arr[20][20];
     scanf("%d", &n);
 
-    for(int i=0; i<n;i++ )
+    for(int i=1; i<=n; i++ )
     {
-        for(int j=0; j<n+1; j++ )
+        for(int j=1; j<=n+1; j++)
         {
             scanf("%d", &arr[i][j]);
         }
