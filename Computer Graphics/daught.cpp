5314e2bd475751af2195c7f8c699f75000615654
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-04-15 12:21:59 +0600

    Update daught.cpp

--- Computer Graphics/daught.cpp
+++ Computer Graphics/daught.cpp
@@ -1,18 +1,26 @@
 #include<bits/stdc++.h>
 #include <windows.h>
-#include <GL\glut.h>
+#include <GL/glut.h>
+
+#define pi (2*acos(0.0))
+
+double cameraHeight;
+double cameraAngle;
+int drawgrid;
+int drawaxes;
+double angle;
 
 
-
-void init()
+struct point
 {
-          glClearColor(0,0,0,0);
-          glMatrixMode(GL_PROJECTION);
-          glOrtho(-15,15,-15,15,-15,15);
-}
+	double x,y,z;
+};
+
 
 void drawAxes()
 {
+	if(drawaxes==1)
+	{
 		glColor3f(1.0, 1.0, 1.0);
 		glBegin(GL_LINES);{
 			glVertex3f( 100,0,0);
@@ -23,196 +31,123 @@ void drawAxes()
 
 			glVertex3f(0,0, 100);
 			glVertex3f(0,0,-100);
+
 		}glEnd();
+	}
 }
 
-
-void DrawSphere()
+void drawSquare()
 {
-    glColor3f(1,0,0);
+   glColor3f(1,0,0);
     glPushMatrix();
-         glTranslated(+8,-6,-6);
+         glTranslated(+80,-60,-60);
         glRotated(60,1,0,0);
         glRotated(90,0,0,1);
-        glutSolidSphere(3,16,16);
+        glutSolidSphere(30,160,16);
     glPopMatrix();
 }
 
-void DrawCone()
+
+void drawCone()
 {
-    glColor3f(1,0,1);
-    glPushMatrix();
-        glTranslated(-8,6,6);
-        glRotated(60,1,0,0);
-        glRotated(60,0,0,1);
-        glutSolidCone(5,5,16,16);
-    glPopMatrix();
-}
-
-void display()
-{
-          glClear(GL_COLOR_BUFFER_BIT);
-          drawAxes();
-          DrawSphere();
-          DrawCone();
-          /*
-          glBegin(GL_QUADS);
-          glColor3f(1, 0, 0);
-          glVertex3f(3, -3, -3);
-          glVertex3f(3,  3, -3);
-          glVertex3f(-3,  3, -3);
-          glVertex3f(-3, -3, -3 );
-          glEnd();
-
-          glBegin(GL_QUADS);
-          glColor3f(1,  1, 1);
-          glVertex3f( 3, -3, 3 );
-          glVertex3f( 3,  3, 3 );
-          glVertex3f(-3,  3, 3);
-          glVertex3f(-3, -3, 3);
-          glEnd();
-
-
-          glBegin(GL_QUADS);
-          glColor3f(1,  0,  1);
-          glVertex3f( 3, -3, -3);
-          glVertex3f( 3,  3, -3);
-          glVertex3f( 3,  3,  3);
-          glVertex3f( 3, -3,  3 );
-          glEnd();
-
-
-          glBegin(GL_QUADS);
-          glColor3f(0,  1,  0);
-          glVertex3f( -3,  -3,  3 );
-          glVertex3f( -3,  3, 3 );
-          glVertex3f( -3,  3, -3 );
-          glVertex3f( -3, -3, -3 );
-          glEnd();
-
-
-          glBegin(GL_QUADS);
-          glColor3f(0,  0,  1);
-          glVertex3f(  3,  3,  3 );
-          glVertex3f(  3,  3, -3 );
-          glVertex3f( -3,  3, -3 );
-          glVertex3f( -3,  3,  3 );
-          glEnd();
-
-
-          glBegin(GL_QUADS);
-          glColor3f( 1,  0,  0);
-          glVertex3f(  3, -3, -3 );
-          glVertex3f(  3, -3,  3 );
-          glVertex3f( -3, -3,  3 );
-          glVertex3f( -3, -3, -3 );
-          glEnd();
-
-          */
-       glFlush();
 
 }
 
 
-void keyboard(int key, int x, int y)
-{
-        switch(key)
-        {
-            case GLUT_KEY_UP:
-                glRotatef(3,1,0,0);
-                break;
 
-            case GLUT_KEY_DOWN:
-                glRotatef(-3,1,0,0);
-                break;
 
-            case GLUT_KEY_LEFT:
-                glRotatef(3,0,1,0);
-                break;
 
-            case GLUT_KEY_RIGHT:
-                glRotatef(-3,0,1,0);
-                break;
 
-            case GLUT_KEY_PAGE_UP:
-                glScalef( 2.0, 2.0, 0.0 );
-                break;
+void display(){
 
-            case GLUT_KEY_PAGE_DOWN:
-                glScalef(0.5, 0.5, 0.0);
-                break;
-        }
-        display();
+	//clear the display
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0,0,0,0);	//color black
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	/********************
+	/ set-up camera here
+	********************/
+	//load the correct matrix -- MODEL-VIEW matrix
+	glMatrixMode(GL_MODELVIEW);
+
+	//initialize the matrix
+	glLoadIdentity();
+
+
+	gluLookAt(200*cos(cameraAngle), 200*sin(cameraAngle), cameraHeight,		0,0,0,		0,0,1);
+
+
+	glMatrixMode(GL_MODELVIEW);
+
+
+
+	///add objects
+	drawAxes();
+
+    glColor3f(0.5,0.5,0.5);
+    drawSquare();
+
+
+	glutSwapBuffers();
 }
 
 
-int main(int argc, char** argv)
-{
-        glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-        glutInitWindowSize(600, 600);
-        glutCreateWindow("3d Cube");
-        init();
-        glutDisplayFunc(display);
-        glutSpecialFunc(keyboard);
-        glutMainLoop();
-        return 0;
+void animate(){
+	angle+=0.05;
+	//codes for any changes in Models, Camera
+	glutPostRedisplay();
 }
 
+void init(){
+	//codes for initialization
+	drawgrid=0;
+	drawaxes=1;
+	cameraHeight=80.0;
+	cameraAngle=1.0;
+	angle=0;
+
+	//clear the screen
+	glClearColor(0,0,0,0);
+
+	/************************
+	/ set-up projection here
+	************************/
+	//load the PROJECTION matrix
+	glMatrixMode(GL_PROJECTION);
+
+	//initialize the matrix
+	glLoadIdentity();
 
 
-
-/*
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
+	//give PERSPECTIVE parameters
+	gluPerspective(80,	1,	1,	1000.0);
+	//field of view in the Y (vertically)
+	//aspect ratio that determines the field of view in the X direction (horizontally)
+	//near distance
+	//far distance
 }
-*/
+
+int main(int argc, char **argv){
+	glutInit(&argc,argv);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);	//Depth, Double buffer, RGB color
+
+	glutCreateWindow("My OpenGL Program");
+
+	init();
+
+	glEnable(GL_DEPTH_TEST);	//enable Depth Testing
+
+	glutDisplayFunc(display);	//display callback function
+	glutIdleFunc(animate);		//what you want to do in the idle time (when no drawing is occuring)
+
+//	glutKeyboardFunc(keyboardListener);
+//glutSpecialFunc(specialKeyListener);
+	///glutMouseFunc(mouseListener);
+
+	glutMainLoop();		//The main loop of OpenGL
+
+	return 0;
+}
