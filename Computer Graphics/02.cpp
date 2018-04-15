fc6ce95a9c52c191fe5c589b3380299a0e85a839
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-04-15 11:13:43 +0600

    Create  02.cpp

---
+++ Computer Graphics/02.cpp
@@ -0,0 +1,167 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#include <windows.h>
+#include <GL/glut.h>
+
+#define pi (2*acos(0.0))
+
+double cameraHeight;
+double cameraAngle;
+int drawgrid;
+int drawaxes;
+double angle;
+
+double fullHand_angle=0.0;
+double backMove_angle=0.0;
+//double upperPart_angle;
+double lowerPart_angle=0.0;
+double palm_angle; //hater talu
+double rigthFinger_angle=0.0;
+double leftFinger_angle=0.0;
+double middleFinger_angle=0.0;
+
+struct point
+{
+	double x,y,z;
+};
+
+
+void drawAxes()
+{
+	if(drawaxes==1)
+	{
+		glColor3f(1.0, 1.0, 1.0);
+		glBegin(GL_LINES);{
+			glVertex3f( 100,0,0);
+			glVertex3f(-100,0,0);
+
+			glVertex3f(0,-100,0);
+			glVertex3f(0, 100,0);
+
+			glVertex3f(0,0, 100);
+			glVertex3f(0,0,-100);
+
+		}glEnd();
+	}
+}
+
+void drawSquare()
+{
+    int radius=10;
+    int scale=5;
+    int translate=-scale*radius;
+
+    glPushMatrix();
+        glTranslatef(-2.4 ,1.2, -6);   //translate=-(5*radius)=-50
+       // glRotatef(-2,4,1.2,-6);
+        glRotatef(60,1,0,0);
+        glScalef(1,1,5);
+        glutWireSphere(10,16,16);
+    glPopMatrix();
+}
+
+
+void drawCone()
+{
+
+}
+
+
+
+
+
+
+void display(){
+
+	//clear the display
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0,0,0,0);	//color black
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	/********************
+	/ set-up camera here
+	********************/
+	//load the correct matrix -- MODEL-VIEW matrix
+	glMatrixMode(GL_MODELVIEW);
+
+	//initialize the matrix
+	glLoadIdentity();
+
+
+	gluLookAt(200*cos(cameraAngle), 200*sin(cameraAngle), cameraHeight,		0,0,0,		0,0,1);
+
+
+	glMatrixMode(GL_MODELVIEW);
+
+
+
+	///add objects
+	drawAxes();
+
+    glColor3f(0.5,0.5,0.5);
+    drawSquare();
+
+
+	glutSwapBuffers();
+}
+
+
+void animate(){
+	angle+=0.05;
+	//codes for any changes in Models, Camera
+	glutPostRedisplay();
+}
+
+void init(){
+	//codes for initialization
+	drawgrid=0;
+	drawaxes=1;
+	cameraHeight=80.0;
+	cameraAngle=1.0;
+	angle=0;
+
+	//clear the screen
+	glClearColor(0,0,0,0);
+
+	/************************
+	/ set-up projection here
+	************************/
+	//load the PROJECTION matrix
+	glMatrixMode(GL_PROJECTION);
+
+	//initialize the matrix
+	glLoadIdentity();
+
+	//give PERSPECTIVE parameters
+	gluPerspective(80,	1,	1,	1000.0);
+	//field of view in the Y (vertically)
+	//aspect ratio that determines the field of view in the X direction (horizontally)
+	//near distance
+	//far distance
+}
+
+int main(int argc, char **argv){
+	glutInit(&argc,argv);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);	//Depth, Double buffer, RGB color
+
+	glutCreateWindow("My OpenGL Program");
+
+	init();
+
+	glEnable(GL_DEPTH_TEST);	//enable Depth Testing
+
+	glutDisplayFunc(display);	//display callback function
+	glutIdleFunc(animate);		//what you want to do in the idle time (when no drawing is occuring)
+
+//	glutKeyboardFunc(keyboardListener);
+//glutSpecialFunc(specialKeyListener);
+	///glutMouseFunc(mouseListener);
+
+	glutMainLoop();		//The main loop of OpenGL
+
+	return 0;
+}
