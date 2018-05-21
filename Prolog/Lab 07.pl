b9e56e92722467b1eabc0014fddcfe49ec8e88cc
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-05-22 02:18:32 +0600

    Update Lab 07.pl

--- Prolog/Lab 07.pl
+++ Prolog/Lab 07.pl
@@ -1,17 +1,53 @@
-user("Rika","1234").
+%domains
+  name =  string
+  pass =  string
+
+ 
+user(name, pass).
+attemp(integer).
+messege(integer).
+
+
+user("Rita","1234").
+
+
 
 getinput(Name,Password):-
         readln(Name),
         readln(Password).
 
+ message(X) :-
+        X=0,
+        write("Login Failed\nPlease Try Again!\n").
+    message(0).       
+
+attemp(X):-
+	getinput(Name,Password),
+        %write(Password),nl,
+        user(Name, Password),
+        write("You logged", Name, "."), nl,
+        user(Name,Password),
+        Y is X-1,
+        messege(Y),
+        attemp(Y),
+    attemp(0).
+
+
 main:-
-        getinput(Name,Password),
-        write(Name),nl,
-        write(Password),nl,
-        user(Name,Password).
+     write("You Try"),nl,
+     attemp(3),
+     write("You Unsuccessful"), nl.
+
+
+     
 
 
 
+
+
+
+
+  
 
 go:-
     write("Enter user name and password\n"),
