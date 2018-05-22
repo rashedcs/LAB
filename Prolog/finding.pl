24bd8a130476e2f6b2e39e9274372331b5846640
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-05-23 05:20:07 +0600

    Update finding.pl

--- Prolog/finding.pl
+++ Prolog/finding.pl
@@ -1,11 +1,20 @@
-go:-
-    read(City),nl,
-    location(City,State),
-    write(City),
-    write("    "),
-    write(State),nl,!,
+user(mehedi,01).
+user(baki,18).
+user(masum,43).
+user(hakkani,32).
+user(rashed,35).
+user(fahim,24).
+user(arafat,26).
 
-location("Jackson","MS").
-location("Washington","DC").
-location("Raleigh","NC").
 
+
+attempt:-
+  	read(Name),nl,
+	user(Name,Password),
+	write(Name),nl,
+	write(Password),nl.
+
+
+
+main:-
+  attempt.
