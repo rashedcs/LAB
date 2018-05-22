b88f5508ea7f541c2d4fdca81f56fbf8f45c5de4
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-05-23 02:01:04 +0600

    Car Problem

--- Prolog/Lab 09.pl
+++ Prolog/Lab 09.pl
@@ -1,1 +1,17 @@
+%domains
+name,color=symbol
+price=integer
+
+%predicates
+car(name,color,price)
+
+
+%clauses
+car(toyota,black,125000).
+car(bmw,white,225000).
+car(tata,black,505000).
+car(maruti,red,455000).
+car(ford,gray,325000).
+
+
 
