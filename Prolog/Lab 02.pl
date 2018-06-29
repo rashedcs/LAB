1aa388d609ada9186b8907774f75bd9512439140
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-06-29 11:59:36 +0600

    Family relationship

--- Prolog/Lab 02.pl
+++ Prolog/Lab 02.pl
@@ -1,3 +1,25 @@
+parent(X,Y) :- father(X,Y).
+parent(X,Y) :- mother(X,Y).
+grandparent(X,Z) :- parent(X,Y), parent(Y,Z).
+ancestor(X,Z) :- parent(X,Z).
+ancestor(X,Y) :- parent(X,Y), ancestor(Y,Z).
+sibling(X,Y) :- mother(M,X), mother(M,Y), 
+                father(F,X), father(F,Y), X \= Y.
+cousin(X,Y) :- parent(U,X), parent(V,Y), sibling(U,V).
+
+father(albert, jeffrey).
+mother(alice, jeffrey).
+father(albert, george).
+mother(alice, george).
+father(john, mary).
+mother(sue, mary).
+father(george, cindy).
+mother(mary, cindy).
+father(george, victor).
+mother(mary, victor).
+
+
+
 parent(raihan,safi).
 parent(jui, safi).
 male(raihan).
