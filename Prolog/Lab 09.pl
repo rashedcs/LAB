1cb65c32ba2751b16459d1491a1561a85ffa836b
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-06-29 19:49:18 +0600

    Car problem

--- Prolog/Lab 09.pl
+++ Prolog/Lab 09.pl
@@ -7,7 +7,7 @@ car(name,color,company,price)
 
 
 %clauses
-car(toyota,black,tata,125000).
+car(toyota,tata,black,125000).
 car(bmw, maruti,white,225000).
 car(swift,maruti,black,505000).
 car(van,maruti,red,455000).
@@ -15,3 +15,9 @@ car(fiat,fiat,gray,325000).
 
 
 
+Goals:
+?- car(bmw,_,_,_).
+true.
+
+?- car(bmw,_,_,_).
+true.
