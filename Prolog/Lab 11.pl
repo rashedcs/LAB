857a425d4b743439f389d97dee989a3d2b6af435
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-06-29 11:45:46 +0600

    Searching student

--- Prolog/Lab 11.pl
+++ Prolog/Lab 11.pl
@@ -1,3 +1,33 @@
+%First Program :
+
+student(mehedi,01).
+student(baki,18).
+student(fahim,24).
+student(arafat,26).
+student(hakkani,32).
+student(rashed,35).
+student(biswas,42).
+student(masum,43).
+student(nazmul,48).
+
+
+
+attempt:-
+  	read(Name),nl,
+	student(Name,Roll),
+	write(Name),nl,
+	write(Roll),nl.
+
+
+
+main:-
+  attempt.
+
+
+
+
+%Second Program :
+
 student(symbol,symbol, integer)
        choice(integer)
 
@@ -31,4 +61,6 @@ student(symbol,symbol, integer)
        %write("Enter your choice : "),nl,
         read(CH),
         choice(CH). 
+
+
 
