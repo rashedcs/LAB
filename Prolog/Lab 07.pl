2c6ea413d91a96d57d5946da8d99229b16dd24d2
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-06-30 09:14:42 +0600

    Update Lab 07.pl

--- Prolog/Lab 07.pl
+++ Prolog/Lab 07.pl
@@ -28,4 +28,32 @@ main:-
   attempt(5).
 
 
+
+
+/* Using repeat predicate */
+user(mehedi,123).
+user(baki,456).
+user(masum,456).
+user(hakkani,123).
+
+getinput(Name,Password) :-
+	read(Name),nl,
+	read(Password),nl,
+	user(Name,Password).
+
+attempt :-
+	getinput(_,_),
+	write("You are now logged on."),nl.
+
+attempt :-
+    repeat,
+	write("Sorry, you are not permitted access."),nl,
+	write("Please try again."),nl,
+	getinput(_,_),
+	write("You are now logged on."),nl.
+
+
+
+
+
 %Daught : https://ideone.com/lEZmXy  
