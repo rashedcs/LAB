b3ebef5865cf2d68db29ab28717f983a67611158
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-07-01 21:46:08 +0600

    Chair

--- Computer Graphics/Chair.cpp
+++ Computer Graphics/Chair.cpp
@@ -2,35 +2,36 @@
 #include<windows.h>
 #include<GL/glut.h>
 #include<bits/stdc++.h>
-#define degree 3.1416
+
 
 
 void init()
 {
-    glClearColor(0, 0, 0, 0);
-	glViewport(0, 0, 500, 500);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-50, 50, -50, 50, -50, 50);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+          glClearColor(0,0,0,0);
+          glMatrixMode(GL_PROJECTION);
+	  glOrtho(-50, 50, -50, 50, -50, 50);
 }
-void drawCube(GLfloat baseX, GLfloat baseY, GLfloat baseZ,GLfloat l, GLfloat w, GLfloat h, GLfloat r, GLfloat g, GLfloat b){
-    glBegin(GL_QUADS);
 
-    glColor3f(r, g, b);     ///Front
+void drawCube(GLfloat baseX, GLfloat baseY, GLfloat baseZ,GLfloat l, GLfloat w, GLfloat h, GLfloat r, GLfloat g, GLfloat b)
+{
+    glBegin(GL_QUADS);
+	
+     ///Front
+    glColor3f(r, g, b);
     glVertex3f(baseX,baseY,baseZ);
     glVertex3f(baseX+l,baseY,baseZ);
     glVertex3f(baseX+l,baseY+h,baseZ);
     glVertex3f(baseX,baseY+h,baseZ);
 
-    glColor3f(r, g, b);      /// Back
+     /// Back
+    glColor3f(r, g, b);
     glVertex3f(baseX,baseY,baseZ+w);
     glVertex3f(baseX+l,baseY,baseZ+w);
     glVertex3f(baseX+l,baseY+h,baseZ+w);
     glVertex3f(baseX,baseY+h,baseZ+w);
 
-    glColor3f(r, g, b);      /// Left
+     /// Left
+    glColor3f(r, g, b);
     glVertex3f(baseX,baseY,baseZ);
     glVertex3f(baseX,baseY,baseZ+w);
     glVertex3f(baseX,baseY+h,baseZ+w);
@@ -57,48 +58,56 @@ void drawCube(GLfloat baseX, GLfloat baseY, GLfloat baseZ,GLfloat l, GLfloat w, 
     glVertex3f(baseX+l,baseY+h,baseZ);
     glVertex3f(baseX+l,baseY+h,baseZ+w);
     glVertex3f(baseX,baseY+h,baseZ+w);
+	
     glEnd();
 }
+
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    drawCube(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 5.0f, 0.0f,0.0f,1.0f);
-    drawCube(4.0f, 0.0f, 0.0f, 1.0f, 1.0f, 5.0f, 0.0f,1.0f,1.0f);
-    drawCube(4.0f, 0.0f, 4.0f, 1.0f, 1.0f, 5.0f, 1.0f,0.0f,0.0f);
-    drawCube(0.0f, 0.0f, 4.0f, 1.0f, 1.0f, 5.0f, 0.0f,1.0f,0.0f);
-    drawCube(0.0f, 5.0f, 0.0f, 5.0f, 5.0f, 1.0f, 1.0f,1.0f,0.0f);
-    drawCube(0.0f, 6.0f, 0.0f, 1.0f, 5.0f, 5.0f, 1.0f,0.5f,0.5f);
+    drawCube(0.0, 0.0, 0.0, 1.0, 1.0, 5.0, 0.0,0.0,1.0);
+    drawCube(4.0, 0.0, 0.0, 1.0, 1.0, 5.0, 0.0,1.0,1.0);
+    drawCube(4.0, 0.0, 4.0, 1.0, 1.0, 5.0, 1.0,0.0,0.0);
+    drawCube(0.0, 0.0, 4.0, 1.0, 1.0, 5.0, 0.0,1.0,0.0);
+    drawCube(0.0, 5.0, 0.0, 5.0, 5.0, 1.0, 1.0,1.0,0.0);
+    drawCube(0.0, 6.0, 0.0, 1.0, 5.0, 5.0, 1.0,0.5,0.5);
     glutSwapBuffers();
 }
 
 
-void nKeys(unsigned char key, int x, int y)
+void specialKeys(unsigned char key, int x, int y)
 {
     switch(key)
     {
         case 'd':
-            glRotatef(30*degree, 1, 0, 0);
+            glRotatef(10, 1, 0, 0);
             break;
         case 'a':
-            glRotatef(-30*degree, 1, 0, 0);
+            glRotatef(-10, 1, 0, 0);
             break;
         case 'w':
-            glRotatef(30*degree, 0, 1, 0);
+            glRotatef(10, 0, 1, 0);
             break;
         case 's':
-            glRotatef(-30*degree, 0, 1, 0);
+            glRotatef(-10, 0, 1, 0);
             break;
         case 'x':
-            glRotatef(30*degree, 0, 0, 1);
+            glRotatef(10, 0, 0, 1);
             break;
 
         case 'z':
-            glRotatef(-30*degree, 0, 0, 1);
+            glRotatef(-10, 0, 0, 1);
             break;
+
+        case 't':
+            glTranslatef(2.0, 0, 0);
+            break;
+
         case '+':
             glScalef(2.0f, 2.0f, 2.0f);
             break;
+
         case '-':
             glScalef(0.75f, 0.75f, 0.75f);
             break;
@@ -112,34 +121,21 @@ void nKeys(unsigned char key, int x, int y)
 
 
 
-
-
-
-
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(500, 500);
-    glutCreateWindow("Room");
+    glutCreateWindow("3D Chair");
+	
     init();
+	
     glEnable(GL_DEPTH_TEST);
     glutDisplayFunc(display);
     glutIdleFunc(display);
-    glutKeyboardFunc(nKeys);
+    glutKeyboardFunc(specialKeys);
     glutMainLoop();
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
