0c05aa816e013b001f9aeabe078f57a099a41c57
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-07-01 08:54:13 +0600

    Newton Backward

--- Numerical Analysis/11.cpp
+++ Numerical Analysis/11.cpp
@@ -1,12 +1,12 @@
  #include<bits/stdc++.h>
     using namespace std;
 
-    double newton(double x[], double y[],  int n,  double f)
+    double newton(double x[], double y[],  int n,  double xn)
     {
              double h=x[1]-x[0];
              double s=1;
-             double p=(f-x[1])/h;
-             double sum=y[0];
+             double p=(xn-x[1])/h;
+             double sum=y[n-1];
 
              for(int i=1; i<=n-1; i++)
              {
@@ -14,23 +14,23 @@
                     {
                           y[j] = y[j+1] - y[j];
                     }
-                    s *= (p-i+1)/i;
-                    sum += s*y[0];
+                    s *= (p+i-1)/i;
+                    sum += s*y[n-1];
              }
              return sum;
     }
 
     int main()
     {
-              double  f, x[20], y[20];
+              double  xn, x[20], y[20];
               int n;
               scanf("%d",&n);
               for(int i=0; i<n; i++)
               {
                   scanf("%lf %lf",&x[i], &y[i]);
               }
-              scanf("%lf",&f);
-              printf("%lf",newton(x, y, n, f));
+              scanf("%lf",&xn);
+              printf("%lf",newton(x, y, n, xn));
 
               return 0;
     }
