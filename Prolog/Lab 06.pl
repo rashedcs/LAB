f9d0b1fa18a321e5741b0db7c58fb2868dd94518
Rashedul Islam <rashedcs@users.noreply.github.com>
2018-07-14 20:38:48 +0600

    sum range

--- Prolog/Lab 06.pl
+++ Prolog/Lab 06.pl
@@ -10,3 +10,20 @@ sum(N,R) :-
 Goal :
 ?- sum(5,R).
    15
+   
+   
+Sum Range : M to N.
+
+sumFrom(X ,Y, S) :- X>Y, !, write('Start should not be greater than End').
+sumFrom(X ,Y, S) :- X=Y, S=X.
+sumFrom(X ,Y, S) :- 
+        Next is X+1,
+        sumFrom(Next, Y, Sum1),
+        S is Sum1+X.   
+
+?- sumForm(5,10,S).
+Correct to: "sumFrom(5,10,S)"? yes
+S = 45 
+
+
+
