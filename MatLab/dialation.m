38680c1eb52ae5287e3e965a208e075b6cb610ae
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-05 08:36:42 +0600

    dialation

--- MatLab/dialation.m
+++ MatLab/dialation.m
@@ -1,32 +1,25 @@
-s=imread('download.jpg');
-
+s=imread('babu.jpg');
 g=im2bw(s);
 %b=input('enter the structuring element: ');
 b = [0 1 0; 1 1 1 ; 0 1 0];
 [p q]=size(b);
-  figure, imshow(g)
-  [m n]=size(g);
-  temp= zeros(m,n);
-   for i=1:m
-       for j=1:n
-           if (g(i,j)==1)
-        
-             for k=1:p
-       for l=1:q
-          if(b(k,l)==1)
-           c=i+k;
-           d=j+l;
-           temp(c,d)=1;
-          
-          end
-       end
-             end
+%figure, imshow(g)
+[m n]=size(g);
+temp= zeros(m,n);
+for i=1:m
+   for j=1:n
+      if (g(i,j)==1)    
+         for k=1:p
+            for l=1:q
+              if(b(k,l)==1)
+               c=i+k;
+               d=j+l;
+               temp(c,d)=1;
+            end
            end
-           
-       end
+         end
+      end
    end
-   
-subplot(2,1,1)
-imshow(g),title('orginal image');
- subplot(2,1,2)
-imshow(temp),title('dialation image');
+end
+subplot(2,1,1), imshow(g),title('orginal image');
+subplot(2,1,2), imshow(temp),title('dialation image');
