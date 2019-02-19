3a4591ca1a9e06e4a5632d94626521f5ed34b583
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 15:32:18 +0600

    Update diagonalimage.m

--- MatLab/diagonalimage.m
+++ MatLab/diagonalimage.m
@@ -1,14 +1,15 @@
 im = imread('C:\Users\Rashed\Downloads\1.jpg');
 im = rgb2gray(im);
-subplot(2,1,1), imshow(im),title('orginal image');
+subplot(2,1,1), imshow(im), title('Orginal image');
 
-[x,y] = size(im)
-for i = 1: x
-    for j = 1: y
+[x, y] = size(im);
+
+for i = 1:1:x
+    for j = 1:1:y
          if (i == j)           im(i,j) = 255;
          elseif (i+j == y+1)   im(i,j) = 255;
          end
     end
 end
 
-subplot(2,1,2), imshow(im), title('diagonal mark image');
+subplot(2,1,2), imshow(im), title('Diagonal mark image');
