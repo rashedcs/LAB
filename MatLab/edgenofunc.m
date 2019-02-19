993b311be859b0ec5464ef8f6aa6615618736852
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 17:58:10 +0600

    Update edgenofunc.m

--- MatLab/edgenofunc.m
+++ MatLab/edgenofunc.m
@@ -1,5 +1,5 @@
+%without built in function
 a= imread('download.jpg');
-
 b  = zeros(size(a,1),size(a,2))
 b = .298*a(:,:,1)+.587*a(:,:,2)+.114*a(:,:,3)
 subplot(2,1,1),imshow(b),title('Input image');
@@ -22,5 +22,21 @@ for i=1:1:r-2,
 end
 
 out = uint8(outim);
-subplot(2,1,2)
-imshow(out),title('edge detect');
+subplot(2,1,2), imshow(out),title('edge detect');
+
+
+
+%with built in function
+a= imread('download.jpg');
+b  = zeros(size(a,1),size(a,2))
+b = .298*a(:,:,1)+.587*a(:,:,2)+.114*a(:,:,3)
+subplot(3,1,1),imshow(b),title('Input image');
+
+h=[-1 -1 -1; 0 0 0; 1 1 1];
+h = h.';
+
+c=imfilter(b,h);
+subplot(3,1,2),imshow(c),title('edge detected image');
+
+d = edge(b,'Prewitt');
+subplot(3,1,3),imshow(d),title('edge detected image2');
