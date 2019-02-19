34facd2163af09a42490023e0cc5f6eb86df714d
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 18:24:48 +0600

    Update gussrep.m

--- MatLab/gussrep.m
+++ MatLab/gussrep.m
@@ -35,3 +35,16 @@ out = uint8(outim);
 
 subplot(3,1,3)
 imshow(out),title('filter image');
+
+
+%without function
+%Gaussian filter using MATLAB built_in function
+%Read an Image
+Img = imread('tarek.jpg');
+A = imnoise(Img,'Gaussian',0.04,0.003);
+%Image with noise
+figure(1),imshow(A);
+
+H = fspecial('Gaussian',[9 9],1.76);
+GaussF = imfilter(A,H);
+figure(2),imshow(GaussF);
