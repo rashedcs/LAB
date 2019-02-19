584b0377509da8ae6f83291846cd7be2df00672e
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 18:02:01 +0600

    Update dialation.m

--- MatLab/dialation.m
+++ MatLab/dialation.m
@@ -23,3 +23,14 @@ end
 
 subplot(2,1,1), imshow(g),title('orginal image');
 subplot(2,1,2), imshow(temp),title('dialation image');
+
+
+
+%with function
+a = imread('download.jpg'); 
+g=im2bw(a);
+se=[0 1 0; 1 1 1; 0 1 0];    
+%se=ones(6,1);   
+b=imdilate(g,se);              		
+subplot(1,2,1), imshow(g);title('input image');         		
+subplot(1,2,2), imshow(b);   title('dilated image');    
