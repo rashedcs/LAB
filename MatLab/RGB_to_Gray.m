a28d152340bfca82231b1a4205be420eebd5d02d
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 16:23:06 +0600

    Update RGB_to_Gray.m

--- MatLab/RGB_to_Gray.m
+++ MatLab/RGB_to_Gray.m
@@ -1,20 +1,25 @@
-img=imread('1.jpg');
-subplot(1,2,1);
+img=imread('C:\Users\Rashed\Downloads\1.jpg');
+subplot(1,2,1), imshow(img), title('Binary Input image');
+[x,y,z] = size(img);
 
-imshow(img);
-title('Binary Input image');
-%[x, y] = size(img);
-
-
-for i = 1:size(img,1)
-   for j = 1:size(img,2)
-     % image(i,j)=0.2989*img(i,j,1)+0.5870*img(i,j,2)+0.1140*img(i,j,3);
-     % standard value for gray image
-     %image(i,j)=0.3*img(i,j,1)+0.6*img(i,j,2)+0.1*img(i,j,3);
-     image(i,j)=0.40*img(i,j,1)+0.50*img(i,j,2)+0.30*img(i,j,3);
+for i = 1:1:x
+   for j = 1:1:y
+      img(i,j) = 0.40*img(i,j,1) + 0.50*img(i,j,2) + 0.30*img(i,j,3);
    end
 end
 
-subplot(1,2,2);
-imshow(image);
-title('Gray Scale  image');
+subplot(1,2,2), imshow(img), title('Gray Scale  image');
+
+
+%with function
+i = imread('bby.jpg')
+
+subplot(2,1,1);
+imshow(i),title('orginal image')
+
+rgb = zeros(size(i,1),size(i,2))
+
+rgb = .298*i(:,:,1)+.587*i(:,:,2)+.114*i(:,:,3)
+
+subplot(2,1,2)
+imshow(rgb),title('RGB image')
