38db04b3d91c0e9a28da2b96f09428abcc755e9f
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 17:44:26 +0600

    Update negativeimage.m

--- MatLab/negativeimage.m
+++ MatLab/negativeimage.m
@@ -1,3 +1,19 @@
+%with function
+im=imread('C:\Users\Rashed\Downloads\1.jpg');
+subplot(3,1,1), imshow(im), title('orginal image');
+
+[x y] = size(im);
+for i=1:1:x
+    for j=1:1:y
+        im(i,j) = 255-im(i,j);
+    end
+end
+
+subplot(3,1,2), imshow(im), title('Histogram');
+
+
+
+%without function
 im = imread('bby.jpg')
 
 subplot(2,1,1)
@@ -9,6 +25,4 @@ rgb = .298*im(:,:,1)+.587*im(:,:,2)+.114*im(:,:,3)
 neg = 255- double(rgb)
 
 s = uint8(neg)
-
-subplot(2,1,2)
-imshow(s),title('negative image')
+subplot(2,1,2), imshow(s),title('negative image')
