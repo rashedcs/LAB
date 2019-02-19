d9d91133fd9f000ac520a8ad8e8d34d9a4680630
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 09:40:10 +0600

    Update imagepadding.m

--- MatLab/imagepadding.m
+++ MatLab/imagepadding.m
@@ -1,22 +1,18 @@
-im = imread('im2.jpg')
+im = rgb2gray(imread('1.jpg'));
+%im = imread('1.jpg');
+%rgb  = zeros(size(im,1),size(im,2));
+%rgb = .298*im(:,:,1)+.587*im(:,:,2)+.114*im(:,:,3);
+subplot(2,1,1), imshow(im), title('orginal image');
 
-rgb  = zeros(size(im,1),size(im,2))
-rgb = .298*im(:,:,1)+.587*im(:,:,2)+.114*im(:,:,3)
-subplot(2,1,1)
-imshow(rgb),title('orginal image');
-
-[x y] = size(rgb);
-
+[x y] = size(im);
 for i=1:x
     for j=1:y
         if(i<=6 || j<=6 || i>=x-6 || j>=y-6)
         %if(i==1 || i==x || j==y || j==1)
-            rgb(i,j) = 0;
+            im(i,j) = 0;
         end
     end
 end
 
-
-subplot(2,1,2)
-imshow(rgb),title('after pading');
+subplot(2,1,2), imshow(im), title('after pading');
 
