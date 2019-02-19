7c97b6e0501f6c6365103988a7efeadaef5d2320
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 20:30:15 +0600

    Update RGB_to_Gray.m

--- MatLab/RGB_to_Gray.m
+++ MatLab/RGB_to_Gray.m
@@ -1,17 +1,16 @@
+%without buitt in  function
 input=imread('test.jpg');
 subplot(1,2,1), imshow(input), title('RGB Scale image');
 [x,y,~] = size(input);
- 
 for i = 1:1:x
    for j = 1:1:y
       output(i,j) = 0.40*input(i,j,1) + 0.50*input(i,j,2) + 0.30*input(i,j,3);
    end
 end
- 
 subplot(1,2,2), imshow(output), title('Gray Scale  image');
 
 
-%with function
+%with buitt in  function
 i = imread('bby.jpg')
 
 subplot(2,1,1);
