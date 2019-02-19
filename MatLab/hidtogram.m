a0f0b00b97cba039050f3e15e77ef4ed0d291fb0
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-02-19 17:36:41 +0600

    Update hidtogram.m

--- MatLab/hidtogram.m
+++ MatLab/hidtogram.m
@@ -1,20 +1,24 @@
-im =  imread('bby.jpg')
+im=imread('C:\Users\Rashed\Downloads\1.jpg');
 
-subplot(3,1,1)
-imshow(im),title('orginal image')
+subplot(3,1,1), imshow(im),title('orginal image')
 
-d = size(im);
-n = d(1)*d(2);
-pk = zeros(0,255)
+[x y] = size(im);
+%d = size(im);
+%n = d(1)*d(2);
+n = x*y; 
+pk = zeros(0,255);
 
-for i=0:255
+for i=0:1:255
     g = find(im==i)
-    [s a] = size(g)
-    pk(i+1)=s/n
+    [nk a] = size(g)
+    pk(i+1)=nk/n
 end
 
-subplot(3,1,2)
-bar(0:255,3*pk,'stack'),title('Histogram')
+subplot(3,1,2), bar(0:255,10*pk,'stack'),title('Histogram')
+
+
+
+
 
 rgb  = zeros(size(im,1),size(im,2))
 rgb = .298*im(:,:,1)+.587*im(:,:,2)+.114*im(:,:,3)
