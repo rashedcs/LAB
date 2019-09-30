b1396e2e00f8e16183f315722cdf9a3c014d00b1
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-09-30 19:38:06 +0600

    Addition

---
+++ DSP/Lab 01.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void addition(int x[], int m, int y[], int n)
+{
+        int maxim = max(m, n);
+        
+        int z[maxim]={0};
+        
+        for(int i=0;i<maxim; i++)
+        {
+            z[i]=x[i]+y[i];
+            printf("%d ",z[i]);
+        }
+}
+
+
+int main()
+{
+    int m, n, x[100]={0},y[100]={0},z[100]={0};
+  //  printf("Enter the size of 1st sequence:\n");
+    scanf("%d",&m);
+    //printf("Enter the size of 2nd sequence:\n");
+    scanf("%d",&n);
+   // printf("Enter 1st sequence:");
+    
+    for(int i=0;i<m;i++)
+    {
+          scanf("%d",&x[i]);
+    }
+    //printf("Enter 2nd sequence:");
+    for(int i=0;i<n;i++)
+    {
+           scanf("%d",&y[i]);
+
+    }
+    addition(x, m, y, n);
+
+}
+
