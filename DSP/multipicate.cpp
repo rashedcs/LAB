86f4e50b04c502762dd28a78fe4f91b2c906c246
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-10-01 06:05:21 +0600

    Update multipicate.cpp

--- DSP/multipicate.cpp
+++ DSP/multipicate.cpp
@@ -1,57 +1,78 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
+
+void evenOdd(float sig[], int num,int ori, float sig2[], int num2,int ori2)
 {
-    int sn1,sn2,or1,or2;
-
-    cout<<"Size of signal 1 & 2: ";
-    cin>>sn1>>sn2;
-
-    float sig1[sn1],sig2[sn2];
-
-    cout<<"Enter signal 1\n";
-    for(int i=0;i<sn1;i++){
-        cin>>sig1[i];
+    
+   // int num2 = num, ori2=(num-ori)+1;
+   // int sig2[num2]={};
+    /*
+    for(int i=0;i<num;i++)
+    {
+       sig2[i]=sig[i];
     }
-    cout<<"Enter origin of signal 1: ";
-    cin>>or1;
-
-    cout<<"Enter signal 2\n";
-    for(int i=0;i<sn2;i++){
-        cin>>sig2[i];
+    reverse(sig2,sig2+num2);
+  
+    for(int i=0;i<num;i++)
+    {
+       sig2[num-i-1]=sig[i];
     }
-
-    cout<<"Enter origin of signal 2: ";
-    cin>>or2;
-
-    int size=max(sn1-or1,sn2-or2)+max(or1,or2);
-
+     */
+    
+    int size = max(num-ori,num2-ori2) + max(ori,ori2);//Calculate size =left+rig  
     float x1[size]={0},x2[size]={0},res[size]={0};
-    int i=max(or1,or2)-or1,j=0;
-    int temp=i+sn1;
-    for(;i<temp;i++){
-        x1[i]=sig1[j];
-        j++;
+    
+    /* Left Side Calculation */
+    int ix=max(ori,ori2)-ori, temp1=ix+num;
+    for(int i=ix,j=0; i<temp1; i++,j++)
+    {
+        x1[i]=sig[j];
     }
 
-    int k=max(or1,or2)-or2,l=0;
-    int temp2=k+sn2;
-    for(;k<temp2;k++){
+    /* Right Side Calculation */
+    int kx=max(ori,ori2)-ori2, temp2=kx+num2;
+    for(int k=kx,l=0; k<temp2; k++,l++)
+    {
         x2[k]=sig2[l];
-        l++;
     }
-
-    for(int i=0;i<size;i++){
+    
+        for(int i=0;i<size;i++){
         res[i]=x2[i]*x1[i];
     }
 
-    cout<<"Multiplied signal with origin "<<min(or1,or2)<<" \n";
+    cout<<"Added signal with origin "<<max(ori,ori2)<<" \n";
     for(int i=0;i<size;i++){
         cout<<res[i]<<" ";
     }
     cout<<endl;
+}
+
+int main()
+{
+    int num,ori, num2, ori2;
+   // cout<<"Size of signal: ";
+    cin>>num>>num2;
+    float sig[num];
+    float sig2[num2];
+
+   // cout<<"Enter signal\n";
+    for(int i=0;i<num;i++)
+    {
+        cin>>sig[i];
+    }
+   // cout<<"Enter origin of signal 1: ";
+    cin>>ori;
+    
+       // cout<<"Enter signal\n";
+    for(int i=0;i<num2;i++)
+    {
+        cin>>sig2[i];
+    }
+   // cout<<"Enter origin of signal 1: ";
+    cin>>ori2;
+
+    evenOdd(sig,num, ori, sig2,num2, ori2);
 
     return 0;
 }
